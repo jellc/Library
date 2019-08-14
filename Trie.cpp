ff362aa7818b065d8f1f36fa42a365428428c2c6
jellcurrent <euler31415@icloud.com>
2019-08-14 22:36:18 +0900

    Expansion of some methods

--- Trie.cpp
+++ Trie.cpp
@@ -2,83 +2,68 @@
 using namespace std;
 
 
-// verified at https://atcoder.jp/contests/dwacon5th-final/submissions/6315156
+// verified at https://atcoder.jp/contests/dwacon5th-final/submissions/6905246
 
 namespace trie_prep {
-    constexpr size_t nodecnt_max = 1e5 * 15;
+    constexpr size_t nodecnt_max = 1.5e6;
     
     struct node {
         node *par, *lft, *rgt;
         size_t cnt;
-        node(node *_par = nullptr) : par(_par), lft(), rgt(), cnt() {}
-    };
-
-    size_t nodecnt = 0;
-    node stock[nodecnt_max];
-
-    template <size_t dep> 
-    class trie {
-        size_t _kind;
-        node *root;
-        int_fast64_t power[dep]; 
-
-        node *alloc(node *_par) const {
+        node(node *p = nullptr) : par(p), lft(), rgt(), cnt() {}
+        static node *alloc(node *p) {
+            static size_t nodecnt = 0;
+            static node stock[nodecnt_max];
             node *ret = stock + nodecnt++;
-            ret->par = _par;
+            ret->par = p;
             return ret;
         }
+    };
 
-        void binaryexpr(int_fast64_t x, bool bit[]) const {
-            for(size_t i = 0; i < dep; ++i, x >>= 1) {
-                bit[dep - i - 1] = x & 1;
-            }
+    template <size_t dep> 
+    struct trie {
+        size_t _kinds;
+        node *root;
+        int_fast64_t power[dep];
+
+        bool bit(int_fast64_t x, size_t d) const {
+            return x >> (dep - d - 1) & 1;
         }
 
-    public:
-        trie() : _kind(), root(alloc(nullptr))
+        trie() : _kinds(), root(node::alloc(nullptr))
         {
             for(int_fast64_t i = (int)dep - 1, t = 1; i >= 0; --i, t <<= 1) {
                 power[i] = t;
             }
         }
 
-        size_t size() const {
-            return root->cnt;
-        }
+        size_t size() const { return root->cnt; }
 
-        size_t kind() const {
-            return _kind;
-        }
+        size_t kinds() const { return _kinds; }
 
         bool insert(int_fast64_t x) {
             node *p = root;
             p->cnt++;
             bool ret = false;
-            bool bit[dep];
-            binaryexpr(x, bit);
             for(size_t d = 0; d < dep; ++d) {
-                if(bit[d]) {
-                    if(!p->rgt) p->rgt = alloc(p), ret = true;
+                if(bit(x, d)) {
+                    if(!p->rgt) p->rgt = node::alloc(p), ret = true;
                     p = p->rgt;
                 } else {
-                    if(!p->lft) p->lft = alloc(p), ret = true;
+                    if(!p->lft) p->lft = node::alloc(p), ret = true;
                     p = p->lft;
                 }
                 p->cnt++;
             }
-            if(p->cnt == 1) _kind++;
+            if(p->cnt == 1) _kinds++;
             return ret;
         }
 
         size_t erase(int_fast64_t x, size_t y = 1) {
-            node *list[dep + 1];
             node *p = root;
-            list[0] = p;
             y = min(y, p->cnt);
-            bool bit[dep];
-            binaryexpr(x, bit);
-            for(size_t d = 0; d < dep; list[++d] = p) {
-                if(bit[d]) {
+            for(size_t d = 0; d < dep; ++d) {
+                if(bit(x, d)) {
                     if(!p->rgt) return 0;
                     p = p->rgt;
                 } else {
@@ -87,22 +72,34 @@ namespace trie_prep {
                 }
                 y = min(y, p->cnt);
             }
-            for(size_t i = 0; i <= dep; ++i) {
-                p = list[i];
+            if(p->cnt == y) _kinds--;
+            while(p) {
                 p->cnt -= y;
+                p = p->par;
             }
-            if(!p->cnt) _kind--;
             return y;
+        }
+
+        size_t count(int_fast64_t x) const {
+            node *p = root;
+            for(size_t d = 0; d < dep; ++d) {
+                if(bit(x, d)) {
+                    if(!p->rgt) return 0;
+                    p = p->rgt;
+                } else {
+                    if(!p->lft) return 0;
+                    p = p->lft;
+                }
+            }
+            return p->cnt;
         }
 
         int_fast64_t closest(int_fast64_t x) const {
             if(!root->cnt) return -1;
             node *p = root;
-            bool bit[dep];
-            binaryexpr(x, bit);
             int_fast64_t ret = 0;
             for(size_t d = 0; d < dep; ++d) {
-                if(bit[d]) {
+                if(bit(x, d)) {
                     if(p->rgt && p->rgt->cnt) p = p->rgt, ret += power[d];
                     else p = p->lft;
                 } else {
@@ -111,7 +108,7 @@ namespace trie_prep {
                 }
             }
             return ret;
-        } 
+        }
 
         int_fast64_t operator[](size_t idx) const {
             if(idx >= size()) return -1;
@@ -135,133 +132,171 @@ using trie_prep::trie;
 
 
 
-// verified at https://atcoder.jp/contests/dwacon5th-final-open/submissions/4980213
 
-// namespace Trie_prep {
-//     constexpr size_t kind_max = 2;
-//     constexpr size_t nodecnt_max = 5e5 * 31 * 2;
+namespace Trie_prep {
+    constexpr size_t units = 26;
+    constexpr size_t nodecnt_max = 1e6;
 
-//     struct node {
-//         node *par;
-//         node *child[kind_max + 1];  //The index 0 indicates terminating character.
-//         int cnt;
-//         node(node *_par = nullptr) : par(_par),cnt(),child() {}
-//     };
+    struct node {
+        node *par;
+        node *child[units];
+        node *ter;
+        size_t cnt;
+        node(node *p = nullptr) : par(p), child(), ter(), cnt() {}
+        static node *alloc(node *p) {
+            static size_t nodecnt = 0;
+            static node stock[nodecnt_max];
+            node *ret = stock + nodecnt++;
+            *ret = node(p);
+            return ret;
+        }
+    };
 
-//     size_t nodecnt = 0;
-//     node stock[nodecnt_max];
+    template <size_t dep, class T, class seq_t = std::vector<T>> 
+    struct Trie {
+        node *root;
+        const function<size_t(T)> f;
+        const function<T(size_t)> invf;
 
-//     template <class T, class seq_t = std::vector<T>> 
-//     struct Trie {
-//         node *root;
-//         const size_t dep;
-//         const function<int(T)> f;    //The return value must be a positive integer.
-//         const function<T(int)> invf; //The argument must be a positive integer.
+        Trie(const function<size_t(T)> &_f, const function<T(size_t)> &_invf) : root(node::alloc(nullptr)), f(_f), invf(_invf) {}
 
-//         Trie(size_t _dep, const function<int(T)> &_f, const function<T(int)> &_invf) : dep(_dep),f(_f),invf(_invf),root(alloc(nullptr)) {}
+        size_t size() const { return root->cnt; }
 
-//         node *alloc(node *_par) {
-//             node *ret = stock + nodecnt++;
-//             ret->par = _par;
-//             return ret;
-//         }
+        bool insert(const seq_t &s) {
+            node *p = root;
+            bool ret = false;
+            for(size_t d = 0; d <= dep; ++d) {
+                p->cnt++;
+                if(s.size() == d) {
+                    if(!p->ter) {
+                        p->ter = node::alloc(p);
+                        ret = true;
+                    }
+                    p->ter->cnt++;
+                    break;
+                }
+                if(d == dep) break;
+                int x = f(s[d]);
+                if(!p->child[x]) {
+                    p->child[x] = node::alloc(p);
+                    ret = true;
+                }
+                p = p->child[x];
+            }
+            return ret;
+        } 
 
-//         size_t size() const { return root->cnt; }
+        size_t erase(const seq_t &s, size_t k = 1) {
+            node *p = root;
+            k = min(k, p->cnt);
+            for(size_t d = 0; d <= dep && d <= s.size(); ++d) {
+                if(s.size() == d) {
+                    if(!p->ter) return 0;
+                    p = p->ter;
+                } else {
+                    size_t x = f(s[d]);
+                    if(!p->child[x]) return 0;
+                    p = p->child[x];
+                }
+                k = min(k, p->cnt);
+            }
+            while(p) {
+                p->cnt -= k;
+                p = p->par;
+            }
+            return k;
+        }
 
-//         bool insert(const seq_t &x) {
-//             node *p = root;
-//             p->cnt++;
-//             bool ret = false;
-//             for(size_t d = 0; d <= dep; ++d) {
-//                 if(x.size() == d) {
-//                     if(!p->child[0]) {
-//                         p->child[0] = alloc(p);
-//                         ret = true;
-//                     }
-//                     p->child[0]->cnt++;
-//                     return ret;
-//                 }
-//                 int i = f(x[d]);
-//                 if(!p->child[i]) {
-//                     p->child[i] = alloc(p);
-//                     ret = true;
-//                 }
-//                 (p = p->child[i])->cnt++;
-//             }
-//             return ret;
-//         } 
+        size_t erase(size_t idx) {
+            if(idx >= size()) return 0;
+            node *p = root;
+            for(size_t d = 0; d < dep; ++d) {
+                if(p->ter) {
+                    if(idx < p->ter->cnt) {
+                        p = p->ter;
+                        break;
+                    }
+                    idx -= p->ter->cnt;
+                }
+                for(size_t i = 0; i < units; ++i) {
+                    size_t now = p->child[i] ? p->child[i]->cnt : 0;
+                    if(idx < now) {
+                        p = p->child[i];
+                        break;
+                    } else {
+                        idx -= now;
+                    }
+                }
+            }
+            while(p) {
+                p->cnt--;
+                p = p->par;
+            }
+            return 1;
+        }
 
-//         bool erase(const seq_t &x) {
-//             if(!count(x)) return false;
-//             node *p = root;
-//             p->cnt--;
-//             for(size_t d = 0; d < dep && d != x.size(); ++d) {
-//                 if(!--(p->child[f(x[d])]->cnt)) {
-//                     p->child[f(x[d])] = nullptr;
-//                     break;
-//                 }
-//                 p = p->child[f(x[d])];
-//             }
-//             return true;
-//         }
+        size_t count(const seq_t &s) const {
+            node *p = root;
+            for(size_t d = 0; d <= dep && d <= s.size(); ++d) {
+                if(s.size() == d) return p->ter ? p->ter->cnt : 0;
+                size_t x = f(s[d]);
+                if(!p->child[x]) return 0;
+                p = p->child[x];
+            }
+            return 0;
+        }
 
-//         size_t count(const seq_t &x) const {
-//             pair<seq_t,size_t> y = _closest(x);
-//             return x == y.first ? y.second : 0;
-//         }
+        size_t lower_bound(const seq_t &s) const {
+            size_t ret = 0;
+            node *p = root;
+            for(size_t d = 0; d < dep && d < s.size() && p; ++d) {
+                ret += p->ter ? p->ter->cnt : 0;
+                for(size_t x = 0; x < f(s[d]); ++x) {
+                    ret += p->child[x] ? p->child[x]->cnt : 0;
+                }
+                p = p->child[f(s[d])];
+            }
+            return ret;
+        }
 
-//         pair<seq_t,size_t> _closest(const seq_t &x) const {
-//             seq_t ret;
-//             node *p = root;
-//             if(!size()) return make_pair(ret,1);
-//             for(size_t d = 0; d < dep; ++d) {
-//                 size_t i = x.size() > d ? f(x[d]) : 0;
-//                 if(i && p->child[i]) {
-//                     ret.push_back(invf(i));
-//                     p = p->child[i];
-//                 } else {
-//                     for(size_t j = 0; j <= kind_max; ++j) {
-//                         if(p->child[j]) {
-//                             p = p->child[j];
-//                             if(!j) return make_pair(ret,p->cnt);
-//                             ret.push_back(invf(j));
-//                             break;
-//                         }
-//                     }
-//                 }
-//             }
-//             return make_pair(ret,p->cnt);
-//         }
+        size_t upper_bound(const seq_t &s) const {
+            size_t ret = 0;
+            node *p = root;
+            for(size_t d = 0; d < dep && d < s.size() && p; ++d) {
+                ret += p->ter ? p->ter->cnt : 0;
+                for(size_t x = 0; x < f(s[d]); ++x) {
+                    ret += p->child[x] ? p->child[x]->cnt : 0;
+                }
+                p = p->child[f(s[d])];
+            }
+            if(p && s.size() <= dep) {
+                ret += p->ter ? p->ter->cnt : 0;
+            }
+            return ret;
+        }
 
-//         seq_t closest(const seq_t &x) const {
-//             return _closest(x).first;
-//         }
-
-//         seq_t operator[](int idx) const {
-//             node *p = root;
-//             seq_t ret;
-//             for(size_t d = 0; d < dep; ++d) {
-//                 for(size_t j = 0; j <= kind_max; ++j) {
-//                     int tmp = p->child[j] ? p->child[j]->cnt : 0;
-//                     if(idx >= tmp) {
-//                         idx -= tmp;
-//                         if(j == kind_max) return ret;
-//                     } else {
-//                         if(!j) return ret;
-//                         ret.push_back(invf(j));
-//                         p = p->child[j];
-//                         break;
-//                     }
-//                 }
-//             }
-//             return ret;
-//         }
-//     };
-// }
-// using Trie_prep::Trie;
-
-
-
-signed main() {
-    
+        seq_t operator[](size_t idx) const {
+            assert(idx < size());
+            node *p = root;
+            seq_t ret = seq_t();
+            for(size_t d = 0; d < dep; ++d) {
+                if(p->ter) {
+                    if(idx < p->ter->cnt) return ret;
+                    idx -= p->ter->cnt;
+                }
+                for(size_t x = 0; x < units; ++x) {
+                    size_t now = p->child[x] ? p->child[x]->cnt : 0;
+                    if(idx < now) {
+                        ret.push_back(invf(x));
+                        p = p->child[x];
+                        break;
+                    } else {
+                        idx -= now;
+                    }
+                }
+            }
+            return ret;
+        }
+    };
 }
+using Trie_prep::Trie;
