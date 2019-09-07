4fd00c889c811c9af9ea753b5ea09e4c94f3dcb1
jellcurrent <euler31415@icloud.com>
2019-09-08 02:59:06 +0900

    a few minor changes

--- Manacher.cpp
+++ Manacher.cpp
@@ -1,21 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 template <class A>
-vector<int> Manacher(const A &v) {
+std::vector<int> Manacher(const A &v)
+{
     int n = v.size();
-    vector<int> r(n);
-    for(int i = 0, c = 0; i < n; ++i) {
+    std::vector<int> r(n);
+    for(int i = 0, c = 0; i < n; ++i)
+    {
         int l = c * 2 - i;
-        if(l >= 0 && c + r[c] > i + r[l]) {
+        if(l >= 0 && c + r[c] > i + r[l])
+        {
             r[i] = r[l];
-        } else {
+        }
+        else
+        {
             int j = c + r[c] - i;
-            while(i - j >= 0 && i + j < n && v[i - j] == v[i + j]) ++j;
+            while(i - j >= 0 && i + j < n && v[i - j] == v[i + j])
+                ++j;
             r[i] = j;
             c = i;
         }
     }
     return r;
 }
-
