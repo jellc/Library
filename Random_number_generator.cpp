a43d02aac84339d195b9af3cd4bfd2b5f3eb6459
jell <euler31415@icloud.com>
2019-11-27 00:47:56 +0900

    enhanced version of "rand_utility.cpp"

---
+++ Random_number_generator.cpp
@@ -0,0 +1,23 @@
+template <typename num_t>
+class Random_number_generator
+{
+    template <bool is_int, class = void>
+    struct unif_t
+    {
+        std::uniform_int_distribution<num_t> unif;
+        unif_t(num_t lower, num_t upper) : unif(lower, upper - 1) {}
+        num_t operator()(std::mt19937 &engine) { return unif(engine); }
+    };
+    template <class void_t>
+    struct unif_t<false, void_t>
+    {
+        std::uniform_real_distribution<num_t> unif;
+        unif_t(num_t lower, num_t upper) : unif(lower, upper) {}
+        num_t operator()(std::mt19937 &engine) { return unif(engine); }
+    };
+    unif_t<std::is_integral<num_t>::value> unif;
+    std::mt19937 engine;
+  public:
+    Random_number_generator(num_t lower = std::numeric_limits<num_t>::min(), num_t upper = std::numeric_limits<num_t>::max()) : unif(lower, upper), engine(std::random_device{}()) {}
+    num_t operator()() { return unif(engine); }
+};
