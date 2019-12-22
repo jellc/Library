0fdbd60d75c571a21fee691a43d7e7e2195c15d4
jell <euler31415@icloud.com>
2019-12-22 10:52:56 +0900

    new style of segment_tree

---
+++ Lazy_segment_tree.hpp
@@ -0,0 +1,148 @@
+#ifndef LAZY_SEGMENT_TREE_HPP
+#define LAZY_SEGMENT_TREE_HPP
+template <class Monoid, class Actor>
+class lazy_segment_tree
+{
+    using value_type = typename Monoid::value_type;
+    using actor_value_type = typename Actor::value_type;
+    Monoid monoid; Actor actor;
+    const size_t orig_n, ext_n;
+    std::vector<value_type> data;
+    std::vector<actor_value_type> lazy;
+    bool *const flag;
+
+    void eval(size_t k, size_t l, size_t r)
+    {
+        if(!flag[k]) return;
+        actor.act(data[k], lazy[k]);
+        if(r - l > 1)
+        {
+            actor(lazy[k * 2], lazy[k]);
+            actor(lazy[k * 2 + 1], lazy[k]);
+            flag[k * 2] = flag[k * 2 + 1] = true;
+        }
+        lazy[k] = actor.identity(), flag[k] = false;
+    }
+
+    void update(size_t a, size_t b, const actor_value_type &x, size_t k, size_t l, size_t r)
+    {
+        eval(k, l, r);
+        if(b <= l || r <= a) return;
+        if(a <= l && r <= b)
+        {
+            actor(lazy[k], x);
+            flag[k] = true;
+            eval(k, l, r);
+        }
+        else
+        {
+            update(a, b, x, k * 2, l, (l + r) >> 1);
+            update(a, b, x, k * 2 + 1, (l + r) >> 1, r);
+            data[k] = monoid(data[k * 2], data[k * 2 + 1]);
+        }
+    }
+
+    value_type fold(size_t a, size_t b, size_t k, size_t l, size_t r)
+    {
+        if(b <= l || r <= a) return monoid.identity();
+        eval(k, l, r);
+        if(a <= l && r <= b) return data[k];
+        return monoid(fold(a, b, k * 2, l, (l + r) >> 1), fold(a, b, k * 2 + 1, (l + r) >> 1, r));
+    }
+
+    void left_bound(size_t idx, const std::function<bool(const value_type &)> &pred,
+                    size_t k, size_t l, size_t r, value_type &now, size_t &res)
+    {
+        if(idx <= l || r < res) return;
+        eval(k, l, r);
+        if(r <= idx)
+        {
+            value_type nxt = monoid(data[k], now);
+            if(pred(nxt))
+            {
+                res = l, now = nxt;
+                return;
+            }
+        }
+        if(r - l > 1)
+        {
+            left_bound(idx, pred, k << 1 ^ 1, (l + r) >> 1, r, now, res);
+            left_bound(idx, pred, k << 1, l, (l + r) >> 1, now, res);
+        }
+    }
+
+    void right_bound(size_t idx, const std::function<bool(const value_type &)> &pred,
+                     size_t k, size_t l, size_t r, value_type &now, size_t &res)
+    {
+        if(idx >= r || l > res) return;
+        eval(k, l, r);
+        if(l >= idx)
+        {
+            value_type nxt = monoid(now, data[k]);
+            if(pred(nxt))
+            {
+                res = r, now = nxt;
+                return;
+            }
+        }
+        if(r - l > 1)
+        {
+            right_bound(idx, pred, k << 1, l, (l + r) >> 1, now, res);
+            right_bound(idx, pred, k << 1 ^ 1, (l + r) >> 1, r, now, res);
+        }
+    }
+
+  public:
+    lazy_segment_tree(size_t n, Monoid _monoid = Monoid(), Actor _actor = Actor())
+        : monoid{_monoid}, actor(_actor), orig_n{n}, ext_n(n > 1 ? 1 << (32 - __builtin_clz(n - 1)) : 1),
+          data(ext_n << 1, monoid.identity()), lazy(ext_n << 1, actor.identity()), flag(new bool[ext_n << 1])
+    {}
+    ~lazy_segment_tree() { delete[] flag; }
+
+    value_type operator[](size_t i) { return fold(i, i + 1); }
+
+    void build(value_type *__first, value_type *__last)
+    {
+        std::copy(__first, __last, &data[ext_n]);
+        for(size_t i = ext_n; i; --i) data[i] = monoid(data[i * 2], data[i * 2 + 1]);
+    }
+
+    template <class iterator>
+    void build(iterator __first, iterator __last)
+    {
+        static_assert(std::is_same<typename std::iterator_traits<iterator>::value_type, value_type>::value, "iterator's value_type should be equal to Monoid's");
+        std::copy(__first, __last, &data[ext_n]);
+        for(size_t i = ext_n - 1; i; --i) data[i] = monoid(data[i * 2], data[i * 2 + 1]);
+    }
+
+    void init(const value_type &x)
+    {
+        for(size_t i = 0; i < ext_n; ++i) data[i + ext_n] = x;
+        for(size_t i = ext_n - 1; i; --i) data[i] = monoid(data[i * 2], data[i * 2 + 1]);
+    }
+
+    void update(size_t a, const actor_value_type &x) { update(a, a + 1, x); }
+
+    void update(size_t a, size_t b, const actor_value_type &x) { update(a, b, x, 1, 0, ext_n); }
+
+    value_type fold(size_t a, size_t b) { return fold(a, b, 1, 0, ext_n); }
+
+    size_t left_bound(size_t i, const std::function<bool(const value_type &)> &pred)
+    {
+        assert(i <= orig_n);
+        size_t res = i;
+        value_type now = monoid.identity();
+        left_bound(i, pred, 1, 0, ext_n, now, res);
+        return res;
+    }
+
+    size_t right_bound(size_t i, const std::function<bool(const value_type &)> &pred)
+    {
+        assert(i < orig_n);
+        size_t res = i;
+        value_type now = monoid.identity();
+        right_bound(i, pred, 1, 0, ext_n, now, res);
+        return res < orig_n ? res : orig_n;
+    }
+}; //class lazy_segment_tree
+#endif
---
+++ Segment_tree.hpp
@@ -0,0 +1,137 @@
+#ifndef SEGMENT_TREE_HPP
+#define SEGMENT_TREE_HPP
+template <class Monoid>
+class segment_tree
+{
+    class uniqueue
+    {
+        size_t *const que, *qbegin, *qend;
+        bool *const inque;
+      public:
+        uniqueue(size_t n) : que(new size_t[n]), qbegin(que), qend(que), inque(new bool[n]{}) {}
+        ~uniqueue() { delete[] que; delete[] inque; }
+      	void clear() { qbegin = qend = que; }
+        bool empty() const { return qbegin == qend; }
+        bool push(size_t x)
+        {
+            if(inque[x]) return false;
+            return inque[*qend++ = x] = true;
+        }
+        size_t pop()
+        {
+            const size_t res{*qbegin++};
+            inque[res] = false;
+          	if(empty()) clear();
+            return res;
+        }
+    }; // class uniqueue
+
+    using value_type = typename Monoid::value_type;
+    Monoid monoid;
+    const size_t orig_n, ext_n;
+    std::vector<value_type> data;
+    uniqueue que;
+
+    void rebuild()
+    {
+        while(!que.empty())
+        {
+            const size_t f = que.pop(), p = f >> 1;
+            if(p && que.push(p)) data[p] = monoid(data[f], data[f ^ 1]);
+        }
+    }
+
+    void left_bound(size_t idx, const std::function<bool(const value_type &)> &pred,
+                    size_t k, size_t l, size_t r, value_type &now, size_t &res)
+    {
+        if(idx <= l || r < res) return;
+        if(r <= idx)
+        {
+            value_type nxt = monoid(data[k], now);
+            if(pred(nxt))
+            {
+                res = l, now = nxt;
+                return;
+            }
+        }
+        if(r - l > 1)
+        {
+            left_bound(idx, pred, k << 1 ^ 1, (l + r) >> 1, r, now, res);
+            left_bound(idx, pred, k << 1, l, (l + r) >> 1, now, res);
+        }
+    }
+
+    void right_bound(size_t idx, const std::function<bool(const value_type &)> &pred,
+                     size_t k, size_t l, size_t r, value_type &now, size_t &res)
+    {
+        if(idx >= r || l > res) return;
+        if(l >= idx)
+        {
+            value_type nxt = monoid(now, data[k]);
+            if(pred(nxt))
+            {
+                res = r, now = nxt;
+                return;
+            }
+        }
+        if(r - l > 1)
+        {
+            right_bound(idx, pred, k << 1, l, (l + r) >> 1, now, res);
+            right_bound(idx, pred, k << 1 ^ 1, (l + r) >> 1, r, now, res);
+        }
+    }
+
+  public:
+    segment_tree(size_t n, Monoid _monoid = Monoid()) : monoid{_monoid}, orig_n{n}, ext_n(n > 1 ? 1 << (32 - __builtin_clz(n - 1)) : 1), data(ext_n << 1, monoid.identity()), que(ext_n << 1) {}
+
+    void build(value_type *__first, value_type *__last)
+    {
+        std::copy(__first, __last, &data[ext_n]);
+        for(size_t i = ext_n; i; --i) data[i] = monoid(data[i * 2], data[i * 2 + 1]);
+        que.clear();
+    }
+
+    template <class iterator>
+    void build(iterator __first, iterator __last)
+    {
+        static_assert(std::is_same<typename std::iterator_traits<iterator>::value_type, value_type>::value, "iterator's value_type should be equal to Monoid's");
+        std::copy(__first, __last, &data[ext_n]);
+        for(size_t i = ext_n - 1; i; --i) data[i] = monoid(data[i * 2], data[i * 2 + 1]);
+        que.clear();
+    }
+
+    value_type fold(size_t l, size_t r)
+    {
+        rebuild();
+        value_type leftval = monoid.identity(), rightval = monoid.identity();
+        l += ext_n, r += ext_n;
+        while(l < r)
+        {
+            if(l & 1) leftval = monoid(leftval, data[l++]);
+            if(r & 1) rightval = monoid(rightval, data[--r]);
+            l >>= 1, r >>= 1;
+        }
+        return monoid(leftval, rightval);
+    }
+
+    value_type &operator[](size_t i) { return que.push(i += ext_n), data[i]; }
+
+    size_t left_bound(size_t i, const std::function<bool(const value_type &)> &pred)
+    {
+        assert(i <= orig_n), rebuild();
+        size_t res = i;
+        value_type now = monoid.identity();
+        left_bound(i, pred, 1, 0, ext_n, now, res);
+        return res;
+    }
+
+    size_t right_bound(size_t i, const std::function<bool(const value_type &)> &pred)
+    {
+        assert(i < orig_n), rebuild();
+        size_t res = i;
+        value_type now = monoid.identity();
+        right_bound(i, pred, 1, 0, ext_n, now, res);
+        return res < orig_n ? res : orig_n;
+    }
+}; // class segment_tree
+#endif
