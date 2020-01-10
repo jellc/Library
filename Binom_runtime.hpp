2ace8ca5a4ff3d96ef397b3082fdab1599974715
jell <euler31415@icloud.com>
2020-01-11 00:31:57 +0900

    simple draft

---
+++ Binom_runtime.hpp
@@ -0,0 +1,29 @@
+#ifndef BINOM_RUNTIME_HPP
+#define BINOM_RUNTIME_HPP
+#include <vector>
+class binom_runtime
+{
+    const int mod;
+    int n;
+    std::vector<int> _fact, _inv, _invfact;
+  public:
+    binom_runtime(const int _mod) noexcept : mod(_mod), n{2}, _fact{1, 1}, _inv{0, 1}, _invfact{1, 1} {}
+    void build(const int m) noexcept
+    {
+        if(m < n) return;
+        int _n = n;
+        while(m >= n) n <<= 1;
+        _fact.resize(n), _inv.resize(n), _invfact.resize(n);
+        for(int i = _n; i < n; ++i)
+        {
+            _fact[i] = (long long)_fact[i - 1] * i % mod;
+            _inv[i] = mod - (long long)mod / i * _inv[mod % i] % mod;
+            _invfact[i] = (long long)_invfact[i - 1] * _inv[i] % mod;
+        }
+    }
+    long long fact(const int x) noexcept { return build(x), x < 0 ? 0 : _fact[x]; }
+    long long inv(const int x) noexcept { return build(x), x < 0 ? 0 : _inv[x]; }
+    long long invfact(const int x) noexcept { return build(x), x < 0 ? 0 : _invfact[x]; }
+    long long binom(const int x, int y) noexcept { return fact(x) * invfact(y) % mod * invfact(x - y) % mod; }
+};
+#endif
