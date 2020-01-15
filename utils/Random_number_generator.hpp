c9aafb822cafb838473a0a7cfb5c37e80f040604
jell <euler31415@icloud.com>
2020-01-15 21:37:42 +0900

    specification changed

--- utils/Random_number_generator.hpp
+++ utils/Random_number_generator.hpp
@@ -1,6 +1,7 @@
 // #line 2 "Random_number_generator.hpp"
 #ifndef Random_number_generator_hpp
 #define Random_number_generator_hpp
+#include <random>
 
 template <typename num_t>
 class Random_number_generator
@@ -9,7 +10,7 @@ class Random_number_generator
     struct unif_t
     {
         std::uniform_int_distribution<num_t> unif;
-        unif_t(num_t lower, num_t upper) : unif(lower, upper - 1) {}
+        unif_t(num_t lower, num_t upper) : unif(lower, upper) {}
         num_t operator()(std::mt19937 &engine) { return unif(engine); }
     };
     template <class void_t>
@@ -22,8 +23,9 @@ class Random_number_generator
     unif_t<std::is_integral<num_t>::value> unif;
     std::mt19937 engine;
   public:
+    // generate random number in [lower, upper].
     Random_number_generator(num_t lower = std::numeric_limits<num_t>::min(), num_t upper = std::numeric_limits<num_t>::max()) : unif(lower, upper), engine(std::random_device{}()) {}
     num_t operator()() { return unif(engine); }
 };
 
-#endif
+#endif // Random_number_generator_hpp
