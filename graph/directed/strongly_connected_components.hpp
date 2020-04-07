bb5605a2436b66ebe26bd1576e30f6b9189953a1
jell <euler31415@icloud.com>
2020-04-07 13:58:05 +0900

    index renamed

--- graph/directed/strongly_connected_components.hpp
+++ graph/directed/strongly_connected_components.hpp
@@ -1,23 +1,23 @@
-#ifndef Strongly_connected_components_hpp
-#define Strongly_connected_components_hpp
+#ifndef strongly_connected_components_hpp
+#define strongly_connected_components_hpp
 #include <vector>
 
-class Strongly_connected_components
+class strongly_connected_components
 {
     const size_t V;
     std::vector<std::vector<size_t>> adj, mem;
     std::vector<size_t> comp, low;
-    size_t n;
+    size_t cnt;
     bool is_built;
 
   public:
-    Strongly_connected_components(size_t _V) : V(_V), adj(V), comp(V), low(V), is_built() {}
+    strongly_connected_components(size_t _V) : V(_V), adj(V), comp(V), low(V), is_built() {}
 
     // add an edge from the vertex s to the vertex t.
     void add_edge(size_t s, size_t t) { adj[s].emplace_back(t); is_built = false; }
 
     // the number of the components.
-    size_t count() { return build(), n; }
+    size_t count() { return build(), cnt; }
 
     // the number of vertices in the i-th component.
     size_t size(size_t i) { return build(), mem[i].size(); }
@@ -32,10 +32,10 @@ class Strongly_connected_components
     std::vector<std::vector<size_t>> make_dag()
     {
         build();
-        std::vector<std::vector<size_t>> res(n);
+        std::vector<std::vector<size_t>> res(cnt);
         bool *apr = new bool[V]{};
         size_t *stack_ptr = new size_t[V];
-        for(size_t i = 0; i != n; ++i)
+        for(size_t i = 0; i != cnt; ++i)
         {
             size_t *itr = stack_ptr;
             for(size_t s : mem[i]) for(size_t t : adj[s]) if(!apr[comp[t]]) apr[comp[t]] = true, *itr++ = comp[t];
@@ -50,13 +50,13 @@ class Strongly_connected_components
     void build()
     {
         if(is_built) return;
-        is_built = true, n = 0;
+        is_built = true, cnt = 0;
         fill(low.begin(), low.end(), -1);
         fill(comp.begin(), comp.end(), -1);
         size_t *itr = new size_t[V];
         for(size_t v = 0, c = 0; v != V; ++v) affix(v, c, itr);
-        delete[] itr; mem.resize(n);
-        for(size_t v = 0; v != V; ++v) mem[comp[v] = n - 1 - comp[v]].emplace_back(v);
+        delete[] itr; mem.resize(cnt);
+        for(size_t v = 0; v != V; ++v) mem[comp[v] = cnt - 1 - comp[v]].emplace_back(v);
     }
 
     size_t affix(size_t v, size_t &c, size_t* &itr)
@@ -64,9 +64,9 @@ class Strongly_connected_components
         if(~low[v]) return ~comp[v] ? -1 : low[v];
         size_t idx = c++; low[v] = idx; *itr++ = v;
         for(int u : adj[v]) low[v] = std::min(low[v], affix(u, c, itr));
-        if(low[v] == idx) { do { comp[*--itr] = n; } while(*itr != v); ++n; }
+        if(low[v] == idx) { do { comp[*--itr] = cnt; } while(*itr != v); ++cnt; }
         return low[v];
     }
-}; // class Strongly_connected_components
+}; // class strongly_connected_components
 
-#endif // Strongly_connected_components_hpp
+#endif // strongly_connected_components_hpp
