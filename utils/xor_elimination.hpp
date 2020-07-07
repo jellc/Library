ccb029e18fe4182dbcdea799bba68e71dab59902
jell <euler31415@icloud.com>
2020-07-08 02:35:35 +0900

    simple xor elimination

---
+++ utils/xor_elimination.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+template <class expr>
+struct xor_elimination
+{
+    std::vector<expr> base;
+    bool add(expr e)
+    {
+        for(const expr &b : base) if((b ^ e) < e) e ^= b;
+        if(e) base.emplace_back(e);
+        return e;
+    }
+    size_t dim() const { return base.size(); }
+}; // struct binary_elimination
