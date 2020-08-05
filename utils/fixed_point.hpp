ec81d801295270e328f7c6db3d36f26b19afa365
jell <euler31415@icloud.com>
2020-08-06 05:21:07 +0900

    fixed point combinator

---
+++ utils/fixed_point.hpp
@@ -0,0 +1,8 @@
+template <class lambda_type>
+class fixed_point
+{
+    lambda_type func;
+public:
+    fixed_point(lambda_type &&f) : func(std::move(f)) {}
+    template <class... Args> auto operator()(Args &&... args) const { return func(*this, std::forward<Args>(args)...); }
+};
