0c926d2d5e4796ac4915a10cf7c4f7026b0f60cf
jell <euler31415@icloud.com>
2020-08-06 06:10:42 +0900

    verify subset convolution

---
+++ test/library-checker/subset_convolution.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/subset_convolution"
+#include "../../algebra/convolution/subset.hpp"
+#include "../../modulus/compile-time/modint.hpp"
+#include <iostream>
+
+int main()
+{
+    using mint=modint<998244353>;
+    using std::cin;
+    int n; cin>>n;
+    std::vector<mint> a(1<<n),b(1<<n);
+    for(auto &x: a) cin>>x;
+    for(auto &x: b) cin>>x;
+    a=subset_convolute(a,b);
+    for(auto x: a) printf("%d ",x.value());
+    puts("");
+}
