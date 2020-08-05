5ba2af73397fd529f7a00107ef9b81d62084ea66
jell <euler31415@icloud.com>
2020-08-06 05:07:08 +0900

    'chle' and 'chge'

---
+++ utils/chval.hpp
@@ -0,0 +1,3 @@
+#include <functional>
+template <class T, class Comp = std::less<T>> bool chle(T &x, const T &y, Comp comp = Comp()) { return comp(y, x) ? x = y, true : false; }
+template <class T, class Comp = std::less<T>> bool chge(T &x, const T &y, Comp comp = Comp()) { return comp(x, y) ? x = y, true : false; }
