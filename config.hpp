2b2f5cc8ae2d9b3c9bbba312214c75abc4a1b477
jell <euler31415@icloud.com>
2020-08-06 05:02:44 +0900

    config.hpp

---
+++ config.hpp
@@ -0,0 +1,26 @@
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+namespace config
+{
+    const auto start_time{std::chrono::system_clock::now()};
+    int64_t elapsed()
+    {
+        using namespace std::chrono;
+        const auto end_time{system_clock::now()};
+        return duration_cast<milliseconds>(end_time - start_time).count();
+    }
+    __attribute__((constructor)) void setup()
+    {
+        using namespace std;
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout << fixed << setprecision(15);
+        #ifdef _buffer_check
+            atexit([]{ ofstream cnsl("CON"); char bufc; if(cin >> bufc) cnsl << "\n\033[1;35mwarning\033[0m: buffer not empty.\n\n"; });
+        #endif
+    }
+    unsigned cases(void);
+    template <class C> void main() { for(unsigned t = cases(); t; --t) C(); }
+}
+struct solver; int main() { config::main<solver>(); }
