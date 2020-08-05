f43b2b4f6d85ae42689dfaced7842a7345416ec8
jell <euler31415@icloud.com>
2020-08-06 05:08:48 +0900

    read.hpp

---
+++ utils/read.hpp
@@ -0,0 +1,15 @@
+// read with std::cin.
+template <class T = void>
+struct read
+{
+    typename std::remove_const<T>::type value;
+    template <class... types>
+    read(types... args) : value(args...) { std::cin >> value; }
+    operator T() const { return value; }
+};
+template <>
+struct read<void>
+{
+    template <class T>
+    operator T() const { T value; std::cin >> value; return value; }
+};
