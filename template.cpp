8165bf3634c3439dfb32c0221cd5f1f41b992a93
jell <euler31415@icloud.com>
2020-08-14 18:10:57 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,21 +1,22 @@
 #include "gcc_option.hpp"
+#include <bits/stdc++.h>
+#include "extlib.hpp"
 #include "config.hpp"
-#include "alias.hpp"
-#include <ext/rope>
 #include "utils/iostream_overload.hpp"
 #include "utils/read.hpp"
 #include "utils/casefmt.hpp"
 #include "utils/fixed_point.hpp"
 #include "utils/chval.hpp"
 #include "utils/binary_search.hpp"
-struct solver; int main() { config::main<solver>(); }
+#include "alias.hpp"
+namespace workspace { struct solver; } int main() { config::main<workspace::solver>(); }
 unsigned config::cases() {
     // return -1; // not specify
     // return read(); // given
     return 1;
 }
 
-struct solver { // start here!
+struct workspace::solver { // start here!
 
 solver()
 {
