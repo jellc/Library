5d4ba6e39204a3cc8c537e4ed48cbbec30d128f3
jell <euler31415@icloud.com>
2020-08-21 01:11:19 +0900

    verify ext_gcd

---
+++ test/aizu-online-judge/extended_euclid_algorithm.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_E"
+#include "math/ext_gcd.hpp"
+#include "utils/iostream_overload.hpp"
+
+int main()
+{
+    int a,b;
+    std::cin>>a>>b;
+    auto [x,y]=ext_gcd(a,b);
+    if(x>0) x-=b,y+=a;
+    if((long long)(y-x)*2>a+b) x+=b,y-=a;
+    std::cout<<x<<" "<<y<<"\n";
+}
