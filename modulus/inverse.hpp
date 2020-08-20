9f81faca6ee75a85a59549ed78a57e05a44fb1a1
jell <euler31415@icloud.com>
2020-08-21 00:41:21 +0900

    modular inverse

---
+++ modulus/inverse.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "modint.hpp"
+#include <vector>
+template <int_fast64_t mod = 0>
+struct inverse
+{
+    using value_type = modint<mod>;
+    value_type operator()(int n) const
+    {
+        assert(n %= mod);
+        if(n < 0) n += mod;
+        for(int m(inv.size()); m <= n; ++m) inv.emplace_back(mod / m * -inv[mod % m]);
+        return inv[n];
+    }
+private:
+    static std::vector<value_type> inv;
+};
+template <>
+struct inverse<0>
+{
+    using value_type = modint_runtime;
+    value_type operator()(int n) const
+    {
+        int_fast64_t mod = value_type::mod();
+        assert(n %= mod);
+        if(n < 0) n += mod;
+        if(inv.empty()) inv = {1, mod != 1};
+        for(int m(inv.size()); m <= n; ++m) inv.emplace_back(mod / m * -inv[mod % m]);
+        return inv[n];
+    }
+private:
+    static std::vector<value_type> inv;
+};
+template <int_fast64_t mod> std::vector<modint<mod>> inverse<mod>::inv = {1, 1};
+std::vector<modint_runtime> inverse<0>::inv;
+using inverse_runtime = inverse<0>;
