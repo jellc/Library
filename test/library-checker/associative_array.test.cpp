c898fbb68bf5a414bce7ec9afe9da23d9e3162f2
jell <euler31415@icloud.com>
2020-08-25 00:15:06 +0900

    test

--- test/library-checker/associative_array.test.cpp
+++ test/library-checker/associative_array.test.cpp
@@ -1,9 +1,41 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
-#include "utils/hash.hpp"
+// #include "utils/hash.hpp"
 #include "utils/sfinae.hpp"
 #include <random>
+#include <functional>
+#include <ext/pb_ds/assoc_container.hpp>
 
 namespace workspace {
+template <class T> struct hash : std::hash<T> {};
+struct std_hash_combine
+{
+    template <class Key>
+    size_t operator()(size_t seed, const Key &key) const { return seed ^ (std::hash<Key>()(key) + 0x9e3779b9 + (seed << 6) + (seed >> 2)); }
+};
+template <class T1, class T2>
+class hash<std::pair<T1, T2>>
+{
+    std_hash_combine comb;
+public:
+    size_t operator()(const std::pair<T1, T2> &pair) const { return comb(comb(0, pair.first), pair.second); }
+};
+template <class... T>
+class hash<std::tuple<T...>>
+{
+    template <class Tuple, size_t index = std::tuple_size<Tuple>::value - 1> struct tuple_hasher { template <class Comb> static size_t apply(size_t seed, const Tuple &t, Comb comb = Comb()) { return comb(tuple_hasher<Tuple, index - 1>::apply(seed, t, comb), std::get<index>(t)); } };
+    template <class Tuple> struct tuple_hasher<Tuple, 0> { template <class Comb> static size_t apply(size_t seed, const Tuple &t, Comb comb = Comb()) { return comb(seed, std::get<0>(t)); } };
+    std_hash_combine comb;
+public:
+    size_t operator()(const std::tuple<T...> &t) const { return tuple_hasher<std::tuple<T...>>::apply(0, t, comb); }
+};
+template <class Key, class Mapped>
+struct hashmap : public __gnu_pbds::gp_hash_table<Key, Mapped, hash<Key>>
+{
+    using base = __gnu_pbds::gp_hash_table<Key, Mapped, hash<Key>>;
+    size_t count(const Key &key) const { return base::find(key) != base::end(); }
+    template <class... Args> auto emplace(Args&&... args) { return base::insert(typename base::value_type(args...)); }
+};
+template <class Key> using hashset = hashmap<Key, __gnu_pbds::null_type>;
 template <class int_type>
 struct hash<enable_if_trait<int_type, std::is_integral>>
 {
