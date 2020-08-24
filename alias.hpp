c63d2d29e646bed4b31e88d7519dafdff3a64e16
jell <euler31415@icloud.com>
2020-08-25 03:00:39 +0900

    delete 'alias.hpp'

--- alias.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-namespace workspace {
-using namespace std; using namespace __gnu_pbds; using namespace __gnu_cxx;
-using i32 = int_least32_t; using i64 = int_least64_t;
-using p32 = pair<i32, i32>; using p64 = pair<i64, i64>;
-template <class T, class Comp = less<T>> using heap = std::priority_queue<T, vector<T>, Comp>;
-} // namespace workspace
--- template.cpp
+++ template.cpp
@@ -8,8 +8,13 @@
 #include "utils/hash.hpp"
 #include "utils/iostream_overload.hpp"
 #include "utils/read.hpp"
-#include "alias.hpp"
-namespace workspace { struct solver; } int main() { config::main<workspace::solver>(); }
+namespace workspace {
+    using namespace std; using namespace __gnu_pbds; using namespace __gnu_cxx;
+    using i64 = int_least64_t; using p32 = pair<int, int>; using p64 = pair<i64, i64>;
+    template <class T, class Comp = less<T>> using heap = std::priority_queue<T, vector<T>, Comp>;
+    struct solver;
+}
+int main() { config::main<workspace::solver>(); }
 unsigned config::cases() {
     // return -1; // not specify
     // int t; std::cin >> t; return t; // given
