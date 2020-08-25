f32ea420e27d537886029804d201d093b44ed63a
jell <euler31415@icloud.com>
2020-08-25 21:23:23 +0900

    add 'element_type<T>'

--- utils/sfinae.hpp
+++ utils/sfinae.hpp
@@ -2,3 +2,5 @@
 #include <type_traits>
 template <class type, template <class> class trait>
 using enable_if_trait_type = typename std::enable_if<trait<type>::value>::type;
+template <class Container>
+using element_type = std::remove_const_t<std::remove_reference_t<decltype(*std::begin(std::declval<Container&>()))>>;
