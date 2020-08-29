7258233d90df8d7d3f299cacc35e00d8e3134724
jell <euler31415@icloud.com>
2020-08-29 23:04:40 +0900

    remove 'LOCAL' macro

--- gcc_option.hpp
+++ gcc_option.hpp
@@ -1,7 +1,5 @@
 #pragma once
-#ifdef LOCAL
-    #define _GLIBCXX_DEBUG
-#else
+#ifdef ONLINE_JUDGE
     #pragma GCC optimize("O3")
     #pragma GCC target("avx,avx2")
     #pragma GCC optimize("unroll-loops")
