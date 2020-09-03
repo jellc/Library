5d8da1b6b9d2c6585fd055d67bb0f0862969dfdf
jell <euler31415@icloud.com>
2020-09-04 03:08:04 +0900

    google c++ style guide

--- template.cpp
+++ template.cpp
@@ -1,7 +1,8 @@
-#include "gcc_option.hpp"
-#include "gcc_builtin.hpp"
 #include <bits/extc++.h>
+
 #include "config.hpp"
+#include "gcc_builtin.hpp"
+#include "gcc_option.hpp"
 #include "utils/binary_search.hpp"
 #include "utils/casefmt.hpp"
 #include "utils/chval.hpp"
@@ -9,24 +10,27 @@
 #include "utils/hash.hpp"
 #include "utils/iostream_overload.hpp"
 #include "utils/read.hpp"
+
 namespace workspace {
-    constexpr char eol = '\n';
-    using namespace std;
-    using i64 = int_least64_t; using p32 = pair<int, int>; using p64 = pair<i64, i64>;
-    template <class T, class Comp = std::less<T>> using priority_queue = std::priority_queue<T, std::vector<T>, Comp>;
-    template <class T> using stack = std::stack<T, std::vector<T>>;
-    struct solver;
-}
+constexpr char eol = '\n';
+using namespace std;
+using i64 = int_least64_t;
+using p32 = pair<int, int>;
+using p64 = pair<i64, i64>;
+template <class T, class Comp = std::less<T>>
+using priority_queue = std::priority_queue<T, std::vector<T>, Comp>;
+template <class T> using stack = std::stack<T, std::vector<T>>;
+struct solver;
+}  // namespace workspace
 int main() { config::main<workspace::solver>(); }
 unsigned config::cases() {
-    // return -1; // not specify
-    // int t; std::cin >> t; return t; // given
-    return 1;
+  // return -1; // not specify
+  // int t; std::cin >> t; return t; // given
+  return 1;
 }
 
-struct workspace::solver { // start here!
-
-solver()
-{
-    
-}};
+struct workspace::solver {
+  solver() {
+    // start here!
+  }
+};
