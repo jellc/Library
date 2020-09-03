4656db0768234681134ac86aebb81023a8a4122b
jell <euler31415@icloud.com>
2020-09-04 00:31:34 +0900

    delete unnecessary reference

--- combinatorics/binomial.hpp
+++ combinatorics/binomial.hpp
@@ -7,16 +7,12 @@ template <int_fast64_t mod>
 struct binomial<modint<mod>>
 {
     using value_type = modint<mod>;
-    struct mulinv_Op
-    {
-        inverse<value_type> &inv;
-        value_type operator()(value_type f, size_t n) const { return f * inv(n); }
-    };
+    struct mulinv_Op { value_type operator()(const value_type &f, const size_t &n) const { return f * inv(n); } };
     static inverse<value_type> inv;
     static factorial<value_type, mulinv_Op> fact_inv;
     static factorial<value_type> fact;
-    value_type operator()(int n, int k) { return fact_inv(k) * fact_inv(n - k) * fact(n); }
+    value_type operator()(const int &n, const int &k) { return fact_inv(k) * fact_inv(n - k) * fact(n); }
 };
 template <int_fast64_t mod> inverse<modint<mod>> binomial<modint<mod>>::inv;
-template <int_fast64_t mod> factorial<modint<mod>, class binomial<modint<mod>>::mulinv_Op> binomial<modint<mod>>::fact_inv{1, mulinv_Op{binomial<modint<mod>>::inv}};
+template <int_fast64_t mod> factorial<modint<mod>, class binomial<modint<mod>>::mulinv_Op> binomial<modint<mod>>::fact_inv{1};
 template <int_fast64_t mod> factorial<modint<mod>> binomial<modint<mod>>::fact;
