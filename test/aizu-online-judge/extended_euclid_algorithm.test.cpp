48dffda407295a2c10c22861c1828f22e73f6d3c
jell <euler31415@icloud.com>
2020-09-07 04:04:19 +0900

    fix path

--- test/aizu-online-judge/extended_euclid_algorithm.test.cpp
+++ test/aizu-online-judge/extended_euclid_algorithm.test.cpp
@@ -1,13 +1,13 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_E"
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_E"
 #include "number_theory/ext_gcd.hpp"
-#include "utils/iostream_overload.hpp"
+#include "utils/stream.hpp"
 
-int main()
-{
-    int a,b;
-    std::cin>>a>>b;
-    auto [x,y]=ext_gcd(a,b);
-    if(x>0) x-=b,y+=a;
-    if((long long)(y-x)*2>a+b) x+=b,y-=a;
-    std::cout<<x<<" "<<y<<"\n";
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  auto [x, y] = ext_gcd(a, b);
+  if (x > 0) x -= b, y += a;
+  if ((long long)(y - x) * 2 > a + b) x += b, y -= a;
+  std::cout << std::tie(x, y) << "\n";
 }
