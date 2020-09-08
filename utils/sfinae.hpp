efbe6eca6f8899f30a4397ceffab9ef7595c6383
jell <euler31415@icloud.com>
2020-09-09 06:25:45 +0900

    multiplicable_uint

--- utils/sfinae.hpp
+++ utils/sfinae.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <type_traits>
+
 template <class type, template <class> class trait>
 using enable_if_trait_type = typename std::enable_if<trait<type>::value>::type;
+
 template <class Container>
 using element_type = typename std::decay<decltype(
     *std::begin(std::declval<Container&>()))>::type;
+
 template <class T, class = void> struct is_integral_ext : std::false_type {};
 template <class T>
 struct is_integral_ext<
@@ -14,3 +17,15 @@ template <> struct is_integral_ext<__int128_t> : std::true_type {};
 template <> struct is_integral_ext<__uint128_t> : std::true_type {};
 template <class T>
 constexpr static bool is_integral_ext_v = is_integral_ext<T>::value;
+
+template <typename T, typename = void> struct multiplicable_uint {
+  using type = uint_least32_t;
+};
+template <typename T>
+struct multiplicable_uint<T, typename std::enable_if<(2 < sizeof(T))>::type> {
+  using type = uint_least64_t;
+};
+template <typename T>
+struct multiplicable_uint<T, typename std::enable_if<(4 < sizeof(T))>::type> {
+  using type = __uint128_t;
+};
