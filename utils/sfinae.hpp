4c4d786725e84b5a46e1a68168d8ce3f0a751ade
jell <euler31415@icloud.com>
2020-09-09 04:49:26 +0900

    is_integral_ext

--- utils/sfinae.hpp
+++ utils/sfinae.hpp
@@ -5,3 +5,10 @@ using enable_if_trait_type = typename std::enable_if<trait<type>::value>::type;
 template <class Container>
 using element_type = typename std::decay<decltype(
     *std::begin(std::declval<Container&>()))>::type;
+template <class T, class = void> struct is_integral_ext : std::false_type {};
+template <class T>
+struct is_integral_ext<
+    T, typename std::enable_if<std::is_integral<T>::value>::type>
+    : std::true_type {};
+template <> struct is_integral_ext<__int128_t> : std::true_type {};
+template <> struct is_integral_ext<__uint128_t> : std::true_type {};
