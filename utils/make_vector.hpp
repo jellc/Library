fbbaedbe9e0b7a73ec2c450e43e2c2eac855618b
jell <euler31415@icloud.com>
2020-09-11 03:33:31 +0900

    shorten

--- utils/make_vector.hpp
+++ utils/make_vector.hpp
@@ -3,14 +3,13 @@
 namespace workspace {
 template <typename T, size_t N>
 constexpr auto make_vector(size_t* sizes, T const& init = T()) {
-  if constexpr (N) {
+  if constexpr (N)
     return std::vector(*sizes, make_vector<T, N - 1>(std::next(sizes), init));
-  } else {
-    return std::vector(*sizes, init);
-  }
+  else
+    return init;
 }
 template <typename T, size_t N>
 constexpr auto make_vector(const size_t (&sizes)[N], T const& init = T()) {
-  return make_vector<T, N - 1>((size_t*)sizes, init);
+  return make_vector<T, N>((size_t*)sizes, init);
 }
 }  // namespace workspace
