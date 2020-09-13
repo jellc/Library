1f8a530028f304f6c048c6ade15a90092db7f0a1
jell <euler31415@icloud.com>
2020-09-13 23:56:56 +0900

    cpp version check

--- utils/sfinae.hpp
+++ utils/sfinae.hpp
@@ -16,9 +16,10 @@ struct is_integral_ext<
     : std::true_type {};
 template <> struct is_integral_ext<__int128_t> : std::true_type {};
 template <> struct is_integral_ext<__uint128_t> : std::true_type {};
+#if __cplusplus >= 201402
 template <class T>
 constexpr static bool is_integral_ext_v = is_integral_ext<T>::value;
-
+#endif
 template <typename T, typename = void> struct multiplicable_uint {
   using type = uint_least32_t;
 };
