674c7b28d6827d16f293ae97f2af12bdfec798e4
jell <euler31415@icloud.com>
2020-09-13 14:19:55 +0900

    update sfinae syntax

--- number_theory/ext_gcd.hpp
+++ number_theory/ext_gcd.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include <tuple>
-template <class int_type, std::enable_if_t<std::is_integral<int_type>::value, std::nullptr_t> = nullptr>
-constexpr std::pair<int_type, int_type> ext_gcd(int_type a, int_type b)
-{
-    bool neg_a = a < 0, neg_b = b < 0;
-    int_type p{1}, q{}, r{}, s{1}, t{};
-    while(b)
-    {
-        r ^= p ^= r ^= p -= (t = a / b) * r;
-        s ^= q ^= s ^= q -= t * s;
-        b ^= a ^= b ^= a %= b;
-    }
-    return {neg_a ? -p : p, neg_b ? -q : q};
+template <class int_type>
+constexpr typename std::enable_if<std::is_integral<int_type>::value,
+                                  std::pair<int_type, int_type>>::type
+ext_gcd(int_type a, int_type b) {
+  bool neg_a = a < 0, neg_b = b < 0;
+  int_type p{1}, q{}, r{}, s{1}, t{};
+  while (b) {
+    r ^= p ^= r ^= p -= (t = a / b) * r;
+    s ^= q ^= s ^= q -= t * s;
+    b ^= a ^= b ^= a %= b;
+  }
+  return {neg_a ? -p : p, neg_b ? -q : q};
 }
