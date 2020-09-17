206a95d9473f000a85a7f96706c63085c42c617c
jell <euler31415@icloud.com>
2020-09-17 20:09:05 +0900

    updates

--- template.cpp
+++ template.cpp
@@ -1,26 +1,15 @@
 #include <bits/extc++.h>
 
+#include "alias.hpp"
 #include "config.hpp"
-#include "gcc_builtin.hpp"
-#include "gcc_option.hpp"
+#include "option.hpp"
 #include "utils.hpp"
 namespace workspace {
-constexpr char eol = '\n';
-using namespace std;
-using i32 = int_least32_t;
-using i64 = int_least64_t;
-using i128 = __int128_t;
-using u32 = uint_least32_t;
-using u64 = uint_least64_t;
-using u128 = __uint128_t;
-template <class T, class Comp = std::less<T>>
-using priority_queue = std::priority_queue<T, std::vector<T>, Comp>;
-template <class T> using stack = std::stack<T, std::vector<T>>;
 struct solver;
 }  // namespace workspace
 int main() { config::main<workspace::solver>(); }
 unsigned config::cases() {
-  // return -1; // not specified
+  // return -1; // unspecified
   // int t; std::cin >> t; return t; // given
   return 1;
 }
