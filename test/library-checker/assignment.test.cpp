6fd741903d2ed9319c861ae58403af220c2d10c2
jell <euler31415@icloud.com>
2020-09-19 23:59:59 +0900

    reformat

--- test/library-checker/assignment.test.cpp
+++ test/library-checker/assignment.test.cpp
@@ -1,30 +1,27 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/assignment"
-#include "graph/directed/flow/min_cost_flow.hpp"
-#include <cstdio>
 #include <cstdint>
-using i64=int64_t;
+#include <cstdio>
 
-int main()
-{
-    int n; scanf("%d",&n);
-    min_cost_flow<int,i64> mcf(n*2);
-    for(int i=0;i<n;i++)
-    {
-        mcf.supply(i,1);
-        mcf.supply(i+n,-1);
-        for(int j=0;j<n;j++)
-        {
-            int a; scanf("%d",&a);
-            mcf.add_edge(i,j+n,1,a);
-        }
+#include "graph/directed/flow/min_cost_flow.hpp"
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  min_cost_flow<int, int64_t> mcf(n * 2);
+  for (int i = 0; i < n; i++) {
+    mcf.supply(i, 1);
+    mcf.supply(i + n, -1);
+    for (int j = 0; j < n; j++) {
+      int a;
+      scanf("%d", &a);
+      mcf.add_edge(i, j + n, 1, a);
     }
-    printf("%lld\n",mcf.optimal());
-    for(int i=0;i<n;i++)
-    {
-        for(auto &e:mcf[i])
-        {
-            if(!e.cap) printf("%d ",e.dst-n);
-        }
+  }
+  printf("%lld\n", mcf.optimal());
+  for (int i = 0; i < n; i++) {
+    for (auto &e : mcf[i]) {
+      if (!e.cap) printf("%d ", e.dst - n);
     }
-    puts("");
+  }
+  puts("");
 }
