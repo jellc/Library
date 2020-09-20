977d93f90367ac5a74845b4308759785da4f33a9
jell <euler31415@icloud.com>
2020-09-21 04:06:55 +0900

    reformat

--- utils/fixed_point.hpp
+++ utils/fixed_point.hpp
@@ -2,12 +2,13 @@
 #include <utility>
 namespace workspace {
 // specify the return type of lambda.
-template <class lambda_type>
-class fixed_point
-{
-    lambda_type func;
-public:
-    fixed_point(lambda_type &&f) : func(std::move(f)) {}
-    template <class... Args> auto operator()(Args &&... args) const { return func(*this, std::forward<Args>(args)...); }
+template <class lambda_type> class fixed_point {
+  lambda_type func;
+
+ public:
+  fixed_point(lambda_type &&f) : func(std::move(f)) {}
+  template <class... Args> auto operator()(Args &&... args) const {
+    return func(*this, std::forward<Args>(args)...);
+  }
 };
-} // namespace workspace
+}  // namespace workspace
