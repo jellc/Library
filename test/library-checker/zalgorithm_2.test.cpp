746f46e66f37bbac79bc6261d4a3b3f9ce291154
jell <euler31415@icloud.com>
2020-09-22 20:34:56 +0900

    verify rolling hash

---
+++ test/library-checker/zalgorithm_2.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+#include <iostream>
+#include <string>
+
+#include "string/rolling_hash.hpp"
+#include "utils/binary_search.hpp"
+
+int main() {
+  std::string s;
+  std::cin >> s;
+  rolling_hash_table hash(s);
+  for (size_t i = 0; i < size(s); ++i) {
+    if (i) std::cout << " ";
+    std::cout << workspace::binary_search(
+        size_t(0), size(s) + 1, [&](size_t len) -> bool {
+          return hash.substr(0, len) == hash.substr(i, len);
+        });
+  }
+  std::cout << "\n";
+}
