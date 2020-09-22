d75ebd879fa91dbe3c8cf9badfa7e8b53f1fdf68
jell <euler31415@icloud.com>
2020-09-22 16:23:43 +0900

    rolling hash

---
+++ string/rolling_hash.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+#include "utils/random_number_generator.hpp"
+
+struct rolling_hashed {
+  using u64 = uint_least64_t;
+  using u128 = __uint128_t;
+
+  constexpr static u64 mod = (1ull << 61) - 1;
+  static u64 base;
+
+  u64 value = 0;
+  size_t length = 0;
+
+  operator std::pair<u64, size_t>() const { return {value, length}; }
+
+  bool operator==(const rolling_hashed &rhs) const {
+    return value == rhs.value && length == rhs.length;
+  }
+
+  rolling_hashed operator+(const rolling_hashed &rhs) const {
+    return {plus(value, mult(rhs.value, base_pow(length))),
+            length + rhs.length};
+  }
+
+  rolling_hashed operator-(const rolling_hashed &rhs) const {
+    assert(!(length < rhs.length));
+    return {minus(value, mult(rhs.value, base_pow(length - rhs.length))),
+            length - rhs.length};
+  }
+
+  static u64 base_pow(size_t exp) {
+    static std::vector<u64> pow{1};
+    while (pow.size() <= exp) {
+      pow.emplace_back(mult(pow.back(), base));
+    }
+    return pow[exp];
+  }
+
+ private:
+  static u64 plus(u64 lhs, u64 rhs) {
+    return (lhs += rhs) < mod ? lhs : lhs - mod;
+  }
+
+  static u64 minus(u64 lhs, u64 rhs) {
+    return (lhs -= rhs) < mod ? lhs : lhs + mod;
+  }
+
+  static u64 mult(u128 lhs, u64 rhs) {
+    lhs *= rhs;
+    lhs = (lhs >> 61) + (lhs & mod);
+    return lhs < mod ? lhs : lhs - mod;
+  }
+};
+
+rolling_hashed::u64 rolling_hashed::base =
+    random_number_generator<u64>(1 << 30, mod - 1)();
+
+template <class str_type> struct rolling_hash_table {
+  constexpr static size_t npos = -1;
+
+  rolling_hash_table() = default;
+
+  rolling_hash_table(str_type str) {
+    std::reverse(std::begin(str), std::end(str));
+    for (auto &&c : str)
+      suffix.emplace_back(
+          rolling_hashed{static_cast<rolling_hashed::u64>(c) + 1, 1} +
+          suffix.back());
+    std::reverse(suffix.begin(), suffix.end());
+  }
+
+  size_t size() const { return suffix.size() - 1; }
+
+  rolling_hashed substr(size_t pos, size_t n = npos) const {
+    assert(pos < size());
+    return suffix[pos] - suffix[pos + std::min(n, size() - pos)];
+  }
+
+ private:
+  std::vector<rolling_hashed> suffix{{}};
+};
