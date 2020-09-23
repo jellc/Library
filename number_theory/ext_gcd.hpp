0e51c58685522ba676cdada13bd880d3a54be745
jell <euler31415@icloud.com>
2020-09-24 00:53:44 +0900

    fix bug when a<0 or b<0

--- number_theory/ext_gcd.hpp
+++ number_theory/ext_gcd.hpp
@@ -6,12 +6,19 @@ template <class int_type>
 constexpr typename std::enable_if<is_integral_ext<int_type>::value,
                                   std::pair<int_type, int_type>>::type
 ext_gcd(int_type a, int_type b) {
-  bool neg_a = a < 0, neg_b = b < 0;
   int_type p{1}, q{}, r{}, s{1}, t{};
-  while (b) {
-    r ^= p ^= r ^= p -= (t = a / b) * r;
-    s ^= q ^= s ^= q -= t * s;
-    b ^= a ^= b ^= a %= b;
+  if (a < 0) {
+    std::tie(p, q) = ext_gcd(-a, b);
+    p = -p;
+  } else if (b < 0) {
+    std::tie(p, q) = ext_gcd(a, -b);
+    q = -q;
+  } else {
+    while (b) {
+      r ^= p ^= r ^= p -= (t = a / b) * r;
+      s ^= q ^= s ^= q -= t * s;
+      b ^= a ^= b ^= a %= b;
+    }
   }
-  return {neg_a ? -p : p, neg_b ? -q : q};
+  return {p, q};
 }
