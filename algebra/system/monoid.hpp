39ba44600f63ad05c397c15e5dc3000a06acc686
jell <euler31415@icloud.com>
2020-09-27 13:46:39 +0900

    update min/max monoid

--- algebra/system/monoid.hpp
+++ algebra/system/monoid.hpp
@@ -1,29 +1,32 @@
 #pragma once
 #include <limits>
-template <class T>
-struct min_monoid
-{
-    using value_type = T;
-    static T min, max;
-    T value;
-    min_monoid() : value(max) {}
-    min_monoid(const T &value) : value(value) {}
-    operator T() const { return value; }
-    min_monoid operator+(const min_monoid &rhs) const
-    {
-        return value < rhs.value ? *this : rhs;
-    }
+
+namespace workspace {
+template <class T, class E = T> struct min_monoid {
+  using value_type = T;
+  static T min, max;
+  T value;
+  min_monoid() : value(max) {}
+  min_monoid(const T &value) : value(value) {}
+  operator T() const { return value; }
+  min_monoid operator+(const min_monoid &rhs) const {
+    return value < rhs.value ? *this : rhs;
+  }
+  min_monoid operator*(const E &rhs) const;
 };
-template <class T> T min_monoid<T>::min = std::numeric_limits<T>::min();
-template <class T> T min_monoid<T>::max = std::numeric_limits<T>::max();
-template <class T>
-struct max_monoid : min_monoid<T>
-{
-    using base = min_monoid<T>;
-    using base::min_monoid;
-    max_monoid() : base(base::min) {}
-    max_monoid operator+(const max_monoid &rhs) const
-    {
-        return !(base::value < rhs.value) ? *this : rhs;
-    }
+
+template <class T, class E>
+T min_monoid<T, E>::min = std::numeric_limits<T>::min() / 2;
+template <class T, class E>
+T min_monoid<T, E>::max = std::numeric_limits<T>::max() / 2;
+
+template <class T, class E = T> struct max_monoid : min_monoid<T, E> {
+  using base = min_monoid<T, E>;
+  using base::min_monoid;
+  max_monoid() : base(base::min) {}
+  max_monoid operator+(const max_monoid &rhs) const {
+    return !(base::value < rhs.value) ? *this : rhs;
+  }
+  max_monoid operator*(const E &rhs) const;
 };
+}
