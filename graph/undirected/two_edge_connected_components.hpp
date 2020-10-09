15fdb43c3d9692006e8cf8769f5cdaa3d4bc1d21
jell <euler31415@icloud.com>
2020-10-10 02:12:06 +0900

    standardize interface

--- graph/undirected/two_edge_connected_components.hpp
+++ graph/undirected/two_edge_connected_components.hpp
@@ -1,79 +1,83 @@
+#pragma once
 #include <cassert>
 #include <vector>
 
 // instance: an undirected and not necessarily simple graph
-class two_edge_connected_component
-{
-    static constexpr size_t nil = -1;
-    bool made;
-    std::vector<size_t> stack, low, comp;
-    std::vector<std::vector<size_t>> graph, tree, memb;
+class two_edge_connected_component {
+  static constexpr size_t nil = -1;
+  std::vector<size_t> stack, low, comp;
+  std::vector<std::vector<size_t>> graph, tree, memb;
 
-    void make(size_t now, size_t pre)
-    {
-        size_t ord = low[now] = stack.size();
-        stack.emplace_back(now);
-        std::vector<size_t> brid;
-        for(size_t to : graph[now])
-        {
-            if(to == pre) { pre = nil; continue; }
-            if(low[to] == nil) make(to, now);
-            if(low[to] > ord)
-            {
-                brid.emplace_back(to);
-                graph[to].emplace_back(now);
-            }
-            else if(low[now] > low[to]) low[now] = low[to];
-        }
-        brid.swap(graph[now]);
-        if(ord == low[now])
-        {
-            auto pos = stack.end();
-            tree.resize(tree.size() + 1);
-            auto &adjc = tree.back();
-            do
-            {
-                --pos;
-                comp[*pos] = memb.size();
-                for(size_t u : graph[*pos]) adjc.emplace_back(comp[u]);
-            } while(*pos != now);
-            memb.emplace_back(pos, stack.end());
-            stack.erase(pos, stack.end());
-        }
+  void make(size_t now, size_t pre) {
+    size_t ord = low[now] = stack.size();
+    stack.emplace_back(now);
+    std::vector<size_t> brid;
+    for (size_t to : graph[now]) {
+      if (to == pre) {
+        pre = nil;
+        continue;
+      }
+      if (low[to] == nil) make(to, now);
+      if (low[to] > ord) {
+        brid.emplace_back(to);
+        graph[to].emplace_back(now);
+      } else if (low[now] > low[to])
+        low[now] = low[to];
     }
-
-public:
-    two_edge_connected_component(size_t n) : made(false), comp(n), graph(n)
-    {
-        stack.reserve(n), tree.reserve(n), memb.reserve(n);
+    brid.swap(graph[now]);
+    if (ord == low[now]) {
+      auto pos = stack.end();
+      tree.resize(tree.size() + 1);
+      auto &adjc = tree.back();
+      do {
+        --pos;
+        comp[*pos] = memb.size();
+        for (size_t u : graph[*pos]) adjc.emplace_back(comp[u]);
+      } while (*pos != now);
+      memb.emplace_back(pos, stack.end());
+      stack.erase(pos, stack.end());
     }
+  }
 
-    void add_edge(size_t u, size_t v)
-    {
-        assert(u < size()), assert(v < size());
-        graph[u].emplace_back(v), graph[v].emplace_back(u);
-        made = false;
-    }
+ public:
+  two_edge_connected_component(size_t n) : comp(n), graph(n) {
+    stack.reserve(n), tree.reserve(n), memb.reserve(n);
+  }
 
-    void make()
-    {
-        if(made) return;
-        made = true;
-        low.assign(size(), nil);
-        for(size_t v = 0; v != size(); ++v) if(low[v] == nil) make(v, nil);
-    }
+  void add_edge(size_t u, size_t v) {
+    assert(u < size()), assert(v < size());
+    graph[u].emplace_back(v), graph[v].emplace_back(u);
+  }
 
-    size_t size() const { return graph.size(); }
+  void make() {
+    low.assign(size(), nil);
+    for (size_t v = 0; v != size(); ++v)
+      if (low[v] == nil) make(v, nil);
+  }
 
-    size_t size(size_t i) { make(); assert(i < count()); return memb[i].size(); }
+  size_t size() const { return graph.size(); }
 
-    size_t count() { make(); return memb.size(); }
+  size_t size(size_t i) {
+    assert(i < count());
+    return memb[i].size();
+  }
 
-    size_t operator[](size_t v) { make(); assert(v < size()); return comp[v]; }
+  size_t count() const { return memb.size(); }
 
-    const std::vector<size_t> &bridge(size_t v) { make(); assert(v < size()); return graph[v]; }
+  size_t operator[](size_t v) const {
+    assert(v < size());
+    return comp[v];
+  }
 
-    const std::vector<size_t> &component(size_t i) { make(); assert(i < count()); return memb[i]; }
+  const std::vector<size_t> &bridge(size_t v) const {
+    assert(v < size());
+    return graph[v];
+  }
 
-    const std::vector<std::vector<size_t>> &bridge_tree() { make(); return tree; }
-}; // class two_edge_connected_component
+  const std::vector<size_t> &component(size_t i) const {
+    assert(i < count());
+    return memb[i];
+  }
+
+  const std::vector<std::vector<size_t>> &bridge_tree() const { return tree; }
+};  // class two_edge_connected_component
