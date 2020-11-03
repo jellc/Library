e3f49cf1d4221b839c8e8f9d77bad7451d215236
jell <euler31415@icloud.com>
2020-11-03 22:46:30 +0900

    doxygen

--- utils/fixed_point.hpp
+++ utils/fixed_point.hpp
@@ -1,14 +1,29 @@
 #pragma once
 #include <utility>
 namespace workspace {
+
 // specify the return type of lambda.
+/*
+ * @class fixed_point
+ * @brief fixed point combinator.
+ */
 template <class lambda_type> class fixed_point {
   lambda_type func;
 
  public:
-  fixed_point(lambda_type &&f) : func(std::move(f)) {}
+  /*
+   * @brief
+   * @param func 1st arg is callable with the rest of args
+   */
+  fixed_point(lambda_type &&func) : func(std::move(func)) {}
+
+  /*
+   * @brief recursively apply *this to 1st arg of func.
+   * @param args arguments of the recursive method
+   */
   template <class... Args> auto operator()(Args &&... args) const {
     return func(*this, std::forward<Args>(args)...);
   }
 };
+
 }  // namespace workspace
