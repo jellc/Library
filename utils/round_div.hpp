e24189d62b5355acf9218c0cbda4f44b6705a939
jell <euler31415@icloud.com>
2020-11-03 22:07:43 +0900

    round_div

---
+++ utils/round_div.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <cassert>
+
+#include "sfinae.hpp"
+
+namespace workspace {
+
+/*
+ * @fn floor_div
+ * @brief floor of division.
+ * @param x an integer
+ * @param y an integer
+ * @return maximum integer z s.t. z <= x / y
+ * @note y must be nonzero.
+ */
+template <typename T1, typename T2>
+constexpr typename std::enable_if<(is_integral_ext<T1>::value &&
+                                   is_integral_ext<T2>::value),
+                                  typename std::common_type<T1, T2>::type>::type
+floor_div(T1 x, T2 y) {
+  assert(y != 0);
+  if (y < 0) x = -x, y = -y;
+  return x < 0 ? (x - y + 1) / y : x / y;
+}
+
+/*
+ * @fn ceil_div
+ * @brief ceil of division.
+ * @param x an integer
+ * @param y an integer
+ * @return minimum integer z s.t. z >= x / y
+ * @note y must be nonzero.
+ */
+template <typename T1, typename T2>
+constexpr typename std::enable_if<(is_integral_ext<T1>::value &&
+                                   is_integral_ext<T2>::value),
+                                  typename std::common_type<T1, T2>::type>::type
+ceil_div(T1 x, T2 y) {
+  assert(y != 0);
+  if (y < 0) x = -x, y = -y;
+  return x < 0 ? x / y : (x + y - 1) / y;
+}
+
+}  // namespace workspace
