f7d8012f31eafbbbc43a88b2323906a6b2e00b1a
jell <euler31415@icloud.com>
2020-11-03 23:01:02 +0900

    doxygen

--- utils/chval.hpp
+++ utils/chval.hpp
@@ -1,12 +1,31 @@
 #pragma once
 #include <functional>
 namespace workspace {
-template <class T, class Comp = std::less<T>>
-bool chle(T &x, const T &y, Comp comp = Comp()) {
+
+/*
+ * @fn chle
+ * @brief substitute y for x if comp(y, x) is true
+ * @param x reference
+ * @param y const reference
+ * @param comp compare function
+ * @return whether or not x is updated
+ */
+template <class Tp, class Comp = std::less<Tp>>
+bool chle(Tp &x, const Tp &y, Comp comp = Comp()) {
   return comp(y, x) ? x = y, true : false;
 }
-template <class T, class Comp = std::less<T>>
-bool chge(T &x, const T &y, Comp comp = Comp()) {
+
+/*
+ * @fn chge
+ * @brief substitute y for x if comp(x, y) is true
+ * @param x reference
+ * @param y const reference
+ * @param comp compare function
+ * @return whether or not x is updated
+ */
+template <class Tp, class Comp = std::less<Tp>>
+bool chge(Tp &x, const Tp &y, Comp comp = Comp()) {
   return comp(x, y) ? x = y, true : false;
 }
+
 }  // namespace workspace
