8f80f8d4e3a4f806a1386f709fab1f96d2230bde
Hirota Kinoshita <44382551+jellc@users.noreply.github.com>
2020-11-14 01:31:38 +0900

    update path

--- utils.hpp
+++ utils.hpp
@@ -5,7 +5,7 @@
  */
 
 #include "utils/binary_search.hpp"
-#include "utils/casefmt.hpp"
+#include "utils/io/casefmt.hpp"
 #include "utils/chval.hpp"
 #include "utils/coordinate_compression.hpp"
 #include "utils/ejection.hpp"
@@ -13,9 +13,9 @@
 #include "utils/hash.hpp"
 #include "utils/make_vector.hpp"
 #include "utils/random_number_generator.hpp"
-#include "utils/read.hpp"
+#include "utils/io/read.hpp"
 #include "utils/round_div.hpp"
 #include "utils/sfinae.hpp"
-#include "utils/stream.hpp"
+#include "utils/io/stream.hpp"
 #include "utils/trinary_search.hpp"
 #include "utils/wrapper.hpp"
