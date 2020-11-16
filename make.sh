318fc17869cd3fc61c8118eecc87ea60c2bd86d0
jell <euler31415@icloud.com>
2020-11-16 21:36:33 +0900

    lib

--- make.sh
+++ make.sh
@@ -3,9 +3,9 @@
 for dir in `ls -p src/ | grep /`
 do
   header=`echo ${dir%/}`
-  echo "#pragma once" > $header
+  echo "#pragma once" > lib/$header
   for srcf in `find src/$dir* -type f`
   do
-  echo "#include \""$srcf"\"" >> $header
+    echo "#include \""$srcf"\"" >> lib/$header
   done
 done
