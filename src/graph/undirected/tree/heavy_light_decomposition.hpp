2fb9d672a9254f32dd17ba9b220b2cbb9b4377cc
jell <euler31415@icloud.com>
2020-11-16 17:32:10 +0900

    doxygen

--- src/graph/undirected/tree/heavy_light_decomposition.hpp
+++ src/graph/undirected/tree/heavy_light_decomposition.hpp
@@ -1,4 +1,10 @@
 #pragma once
+
+/*
+ * @file heavy_light_decomposition.hpp
+ * @brief Heavy Light Decomposition
+ */
+
 #include <cassert>
 #include <numeric>
 #include <vector>
@@ -34,14 +40,28 @@ class heavy_light_decomposition {
     out[node] = sorted.size();
   }
 
+  bool made() const { return !sorted.empty(); }
+
  public:
   using interval = std::pair<size_t, size_t>;
 
-  heavy_light_decomposition(size_t n) : tree(n), in(n), out(n), head(n) {}
+  heavy_light_decomposition(size_t n = 0) : tree(n), in(n), out(n), head(n) {}
 
+  /*
+   * @fn size
+   * @return The size of the tree.
+   */
   size_t size() const { return tree.size(); }
 
-  size_t size(size_t node) const { return out[node] - in[node]; }
+  /*
+   * @fn size
+   * @param node The root of the subtree
+   * @return The size of the subtree.
+   */
+  size_t size(size_t node) const {
+    assert(made());
+    return out[node] - in[node];
+  }
 
   void add_edge(size_t u, size_t v) {
     assert(u < size());
@@ -52,7 +72,11 @@ class heavy_light_decomposition {
 
   const decltype(tree) &graph() const { return tree; }
 
-  // O(n)
+  /*
+   * @fn make
+   * @brief Run HLD with given root in linear time.
+   * @param root The root node.
+   */
   void make(size_t root) {
     sorted.clear();
     sort_children(root, root);
@@ -60,33 +84,65 @@ class heavy_light_decomposition {
   }
 
   size_t prev_node(size_t node) const {
+    assert(made());
     assert(in[node]);
     return sorted[in[node] - 1];
   }
 
   size_t next_node(size_t node) const {
+    assert(made());
     assert(in[node] + 1 < size());
     return sorted[in[node] + 1];
   }
 
-  size_t index(size_t node) const { return in[node]; }
+  size_t index(size_t node) const {
+    assert(made());
+    return in[node];
+  }
 
-  size_t node(size_t index) const { return sorted[index]; }
+  size_t node(size_t index) const {
+    assert(made());
+    return sorted[index];
+  }
 
-  size_t root() const { return sorted.front(); }
+  /*
+   * @fn root
+   * @return The current root of the tree.
+   */
+  size_t root() const {
+    assert(made());
+    return sorted.front();
+  }
 
-  interval subtree(size_t root) const { return {in[root], out[root]}; }
+  /*
+   * @fn subtree
+   * @param root The root of the subtree.
+   * @return The interval representing the subtree.
+   */
+  interval subtree(size_t root) const {
+    assert(made());
+    return {in[root], out[root]};
+  }
 
   size_t parent(size_t node) const {
+    assert(made());
     return head[node] < size() ? prev_node(node) : head[node] - size();
   }
 
   size_t top(size_t node) const {
+    assert(made());
     return head[node] < size() ? head[node] : node;
   }
 
-  // O(log(n))
+  /*
+   * @fn lca
+   * @brief Get LCA in O(log(size)) time.
+   * @param u 1st node
+   * @param v 2nd node
+   * @return Lowest Common Ancestor of the two.
+   */
   size_t lca(size_t u, size_t v) {
+    assert(made());
     if (in[v] < in[u]) std::swap(u, v);
     if (in[v] < out[u]) return u;
     while (in[u] < in[v]) v = parent(top(v));
@@ -96,6 +152,7 @@ class heavy_light_decomposition {
   // O(log(n))
   std::pair<std::vector<interval>, std::vector<interval>> path_decomposition(
       size_t u, size_t v) const {
+    assert(made());
     if (in[v] < in[u]) std::swap(u, v);
     std::vector<interval> left, right;
     size_t utop = top(u), vtop = top(v);
@@ -114,6 +171,7 @@ class heavy_light_decomposition {
 
   // O(log(n))
   std::vector<interval> path_decomposition(size_t node) const {
+    assert(made());
     auto [left, right] = path_decomposition(root(), node);
     assert(left.size() == 1);
     right.insert(right.begin(), left.front());
