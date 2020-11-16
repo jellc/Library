88bb5b048c73915a9dfc700d7280be3b12641e7d
jell <euler31415@icloud.com>
2020-11-16 22:49:01 +0900

    doxygen

--- src/string/Manacher.hpp
+++ src/string/Manacher.hpp
@@ -1,43 +1,75 @@
-// #line 2 "Manacher.hpp"
-#ifndef Manacher_hpp
-#define Manacher_hpp
+#pragma once
+
+/*
+ * @file Manacher.hpp
+ * @brief Manacher's Algorithm
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
 
-template <class str_type = std::string>
-class Manacher
-{
-    const size_t len;
-    std::vector<size_t> rad;
-    friend std::ostream &operator<<(std::ostream &os, const Manacher &mana)
-    {
-        bool is_front = true;
-        for(size_t r : mana.rad)
-        {
-            if(is_front) is_front = false;
-            else os << " ";
-            os << r;
-        }
-        return os;
-    }
-  public:
-    Manacher(const str_type &str) : len(str.size()), rad(str.size())
-    {
-        for(size_t i = 0, c = 0; i != len; ++i)
-        {
-            int l = c * 2 - i;
-            if(l >= 0 && c + rad[c] > i + rad[l]) rad[i] = rad[l];
-            else
-            {
-                size_t j = c + rad[c] - i;
-                while(i >= j && i + j != len && str[i - j] == str[i + j]) ++j;
-                rad[c = i] = j;
-            }
-        }
-    }
-    size_t size() const { return rad.size(); }
-    size_t operator[](size_t i) const { return rad[i]; }
-}; // class Manacher
+namespace workspace {
 
-#endif // Manacher_hpp
+/*
+ * @class Manacher
+ * @brief Manacher's Algorithm.
+ * @tparam str_type the type of string
+ */
+template <class str_type = std::string> class Manacher {
+  const size_t len;
+  std::vector<size_t> rad;
+
+ public:
+  Manacher(const str_type &str) : len(str.size()), rad(str.size()) {
+    for (size_t i = 0, c = 0; i != len; ++i) {
+      int l = c * 2 - i;
+      if (l >= 0 && c + rad[c] > i + rad[l])
+        rad[i] = rad[l];
+      else {
+        size_t j = c + rad[c] - i;
+        while (i >= j && i + j != len && str[i - j] == str[i + j]) ++j;
+        rad[c = i] = j;
+      }
+    }
+  }
+
+  std::vector<size_t>::const_iterator begin() const { return rad.begin(); }
+
+  std::vector<size_t>::const_iterator end() const { return rad.end(); }
+
+  /*
+   * @fn size
+   * @return length of the string
+   */
+  size_t size() const { return rad.size(); }
+
+  /*
+   * @fn operator[]
+   * @param i index
+   * @return the maximum radius L s.t. the substring (i - L, i + L) is
+   * palindrome
+   */
+  size_t operator[](size_t i) const { return rad[i]; }
+
+  /*
+   * @fn operator<<
+   * @brief print a Manacher object.
+   * @param os ostream
+   * @param manacher a Manacher object
+   * @return os
+   */
+  friend std::ostream &operator<<(std::ostream &os, const Manacher &manacher) {
+    bool is_front = true;
+    for (size_t r : manacher.rad) {
+      if (is_front)
+        is_front = false;
+      else
+        os << " ";
+      os << r;
+    }
+    return os;
+  }
+};
+
+}  // namespace workspace
