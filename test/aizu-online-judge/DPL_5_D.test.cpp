12ac5043b0f9fbd0b41a89218540d82ce523dc91
jell <euler31415@icloud.com>
2020-11-16 23:27:45 +0900

    update path

---
+++ test/aizu-online-judge/DPL_5_D.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DPL_5_D"
+
+#include <cstdio>
+
+#include "src/combinatorics/binomial.hpp"
+
+int main() {
+  using namespace workspace;
+  int n, k;
+  scanf("%d%d", &n, &k);
+  printf("%d\n", binomial<modint<1000000007>>()(n + k - 1, n));
+}
