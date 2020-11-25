5258d15166581458cb7e12c8999f7c34aa041e4c
jell <euler31415@icloud.com>
2020-11-25 21:14:47 +0900

    edit

--- template.cpp
+++ template.cpp
@@ -13,8 +13,8 @@
 int main() { workspace::iterate_main(); }
 
 unsigned workspace::case_number() {
-  // return -1; // unspecified
-  // int t; std::cin >> t; std::cin.ignore(); return t; // given
+  // return -1;  // unspecified
+  // unsigned t; cin >> t, cin.ignore(); return t;  // given
   return 1;
 }
 
