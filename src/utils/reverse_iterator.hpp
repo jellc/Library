ca65fd4074d3b87f16f3c850df6a102b4a23e31f
jell <euler31415@icloud.com>
2020-12-04 02:13:02 +0900

    reverse iterator

---
+++ src/utils/reverse_iterator.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+/*
+ * @file reverse_iterator.hpp
+ * @brief Reverse Iterator
+ */
+
+#include <iterator>
+#include <optional>
+
+namespace workspace {
+
+/*
+ * @class reverse_iterator
+ * @brief Wrapper class for `std::reverse_iterator`.
+ * @see http://gcc.gnu.org/PR51823
+ */
+template <class Iterator>
+class reverse_iterator : public std::reverse_iterator<Iterator> {
+  using base_std = std::reverse_iterator<Iterator>;
+  std::optional<typename base_std::value_type> deref;
+
+ public:
+  using base_std::reverse_iterator;
+
+  constexpr typename base_std::reference operator*() noexcept {
+    if (!deref) {
+      Iterator tmp = base_std::current;
+      deref = *--tmp;
+    }
+    return deref.value();
+  }
+
+  constexpr reverse_iterator &operator++() noexcept {
+    base_std::operator++();
+    deref.reset();
+    return *this;
+  }
+  constexpr reverse_iterator &operator--() noexcept {
+    base_std::operator++();
+    deref.reset();
+    return *this;
+  }
+  constexpr reverse_iterator operator++(int) noexcept {
+    base_std::operator++();
+    deref.reset();
+    return *this;
+  }
+  constexpr reverse_iterator operator--(int) noexcept {
+    base_std::operator++();
+    deref.reset();
+    return *this;
+  }
+};
+
+}  // namespace workspace
