b5781c1fa97f037df2c3585f4f0bc0956c4e2474
jell <euler31415@icloud.com>
2020-12-08 19:06:53 +0900

    format

--- src/combinatorics/factorial.hpp
+++ src/combinatorics/factorial.hpp
@@ -1,17 +1,27 @@
 #pragma once
+
+/*
+ * @brief Factorial
+ */
+
 #include <functional>
 #include <vector>
-template <class T, class Op = std::multiplies<T>>
-class factorial
-{
-    std::vector<T> fact;
-    Op op;
-public:
-    factorial(T init = 1, Op op = Op()) : fact{init}, op{op} {}
-    T operator()(const int &n)
-    {
-        if(n < 0) return 0;
-        for(int m(fact.size()); m <= n; ++m) fact.emplace_back(op(fact.back(), m));
-        return fact[n];
-    }
-}; // class factorial
+
+namespace workspace {
+
+template <class Tp, class Op = std::multiplies<Tp>> class factorial {
+  std::vector<Tp> fact;
+  Op op;
+
+ public:
+  factorial(Tp init = 1, Op op = Op()) : fact{init}, op{op} {}
+
+  Tp operator()(int_least32_t n) {
+    if (n < 0) return 0;
+    for (int_least32_t m(fact.size()); m <= n; ++m)
+      fact.emplace_back(op(fact.back(), m));
+    return fact[n];
+  }
+};
+
+}  // namespace workspace
