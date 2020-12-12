40adedbf6a4113f218a961b3e42f1897fccfa334
jell <euler31415@icloud.com>
2020-12-13 01:06:44 +0900

    int128

--- src/utils/io/istream.hpp
+++ src/utils/io/istream.hpp
@@ -15,52 +15,85 @@
 
 namespace workspace {
 
-/*
- * @class istream
+namespace internal {
+
+template <class Tp, typename = std::nullptr_t> struct istream_helper {
+  istream_helper(std::istream &is, Tp &x) {
+    if constexpr (has_begin<Tp>::value)
+      for (auto &&e : x)
+        istream_helper<typename std::decay<decltype(e)>::type>(is, e);
+    else
+      static_assert(has_begin<Tp>::value, "istream unsupported type.");
+  }
+};
+
+template <class Tp>
+struct istream_helper<
+    Tp,
+    decltype(std::declval<std::decay<decltype(std::declval<std::istream &>() >>
+                                              std::declval<Tp &>())>>(),
+             nullptr)> {
+  istream_helper(std::istream &is, Tp &x) { is >> x; }
+};
+
+#ifdef __SIZEOF_INT128__
+
+template <> struct istream_helper<__int128_t, std::nullptr_t> {
+  istream_helper(std::istream &is, __int128_t &x) {
+    std::string s;
+    is >> s;
+    bool negative = s.front() == '-' ? s.erase(s.begin()), true : false;
+    x = 0;
+    for (char e : s) x = x * 10 + e - '0';
+    if (negative) x = -x;
+  }
+};
+
+template <> struct istream_helper<__uint128_t, std::nullptr_t> {
+  istream_helper(std::istream &is, __uint128_t &x) {
+    std::string s;
+    is >> s;
+    bool negative = s.front() == '-' ? s.erase(s.begin()), true : false;
+    x = 0;
+    for (char e : s) x = x * 10 + e - '0';
+    if (negative) x = -x;
+  }
+};
+
+#endif  // INT128
+
+template <class T1, class T2> struct istream_helper<std::pair<T1, T2>> {
+  istream_helper(std::istream &is, std::pair<T1, T2> &x) {
+    istream_helper<T1>(is, x.first), istream_helper<T2>(is, x.second);
+  }
+};
+
+template <class... Tps> struct istream_helper<std::tuple<Tps...>> {
+  istream_helper(std::istream &is, std::tuple<Tps...> &x) { iterate(is, x); }
+
+ private:
+  template <class Tp, size_t N = 0> void iterate(std::istream &is, Tp &x) {
+    if constexpr (N == std::tuple_size<Tp>::value)
+      return;
+    else
+      istream_helper<typename std::tuple_element<N, Tp>::type>(is,
+                                                               std::get<N>(x)),
+          iterate<Tp, N + 1>(is, x);
+  }
+};
+
+}  // namespace internal
+
+/**
  * @brief A wrapper class for std::istream.
  */
 class istream : public std::istream {
-  template <class Tp, typename = std::nullptr_t> struct helper {
-    helper(std::istream &is, Tp &x) {
-      if constexpr (has_begin<Tp>::value)
-        for (auto &&e : x)
-          helper<typename std::decay<decltype(e)>::type>(is, e);
-      else
-        static_assert(has_begin<Tp>::value, "istream unsupported type.");
-    }
-  };
-
-  template <class Tp>
-  struct helper<
-      Tp,
-      decltype(std::declval<std::decay<decltype(
-                   std::declval<std::istream &>() >> std::declval<Tp &>())>>(),
-               nullptr)> {
-    helper(std::istream &is, Tp &x) { is >> x; }
-  };
-
-  template <class T1, class T2> struct helper<std::pair<T1, T2>> {
-    helper(std::istream &is, std::pair<T1, T2> &x) {
-      helper<T1>(is, x.first), helper<T2>(is, x.second);
-    }
-  };
-
-  template <class... Tps> struct helper<std::tuple<Tps...>> {
-    helper(std::istream &is, std::tuple<Tps...> &x) { iterate(is, x); }
-
-   private:
-    template <class Tp, size_t N = 0> void iterate(std::istream &is, Tp &x) {
-      if constexpr (N == std::tuple_size<Tp>::value)
-        return;
-      else
-        helper<typename std::tuple_element<N, Tp>::type>(is, std::get<N>(x)),
-            iterate<Tp, N + 1>(is, x);
-    }
-  };
-
  public:
+  /**
+   * @brief Wrapped operator.
+   */
   template <typename Tp> istream &operator>>(Tp &x) {
-    helper<Tp>(*this, x);
+    internal::istream_helper<Tp>(*this, x);
     if (std::istream::fail()) {
       static auto once = atexit([] {
         std::cerr << "\n\033[43m\033[30mwarning: failed to read \'"
