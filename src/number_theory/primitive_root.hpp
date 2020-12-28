0f09d6c594821c4f1097a199b96547707412c9ed
jell <euler31415@icloud.com>
2020-12-28 23:08:41 +0900

    support non-prime

--- src/number_theory/primitive_root.hpp
+++ src/number_theory/primitive_root.hpp
@@ -8,48 +8,57 @@
 
 #include "src/utils/sfinae.hpp"
 
+#if __cplusplus >= 201703L
+
+#include <optional>
+
 namespace workspace {
 
 /**
- * @brief Compile time primitive root
+ * @brief Compile time primitive root.
  *
- * @tparam Mod
+ * @tparam __mod A positive integer
+ * @return Minimum positive one if it exists. Otherwise 0.
  */
-template <auto Mod>
-constexpr static
-    typename std::enable_if<(is_integral_ext<decltype(Mod)>::value && Mod > 0),
-                            decltype(Mod)>::type primitive_root = [] {
-      using result_type = decltype(Mod);
-      using int_type = typename multiplicable_uint<result_type>::type;
+template <class Tp>
+constexpr typename std::enable_if<(is_integral_ext<Tp>::value), Tp>::type
+primitive_root(const Tp __mod) noexcept {
+  assert(__mod > 0);
+  using int_type = typename multiplicable_uint<Tp>::type;
 
-      if constexpr (Mod == 2) return static_cast<result_type>(1);
+  int_type __r = __mod, __p[16] = {}, *__q = __p;
+  for (int_type __i = 2; __i <= __r / __i; ++__i) {
+    if (__r % __i) continue;
+    *__q++ = __i;
+    while (!(__r % __i)) __r /= __i;
+  }
+  if (__r != 1) *__q++ = __r;
 
-      auto __m = Mod - 1;
-      while (!(__m & 1)) __m >>= 1;
+  int_type __tot = __mod;
+  for (__q = __p; *__q; *__q++ = 0) (__tot /= *__q) *= *__q - 1;
+  __r = __tot, __q = __p + 1, __p[0] = 1;
+  for (int_type __i = 2; __i <= __r / __i; ++__i) {
+    if (__r % __i) continue;
+    *__q++ = __i;
+    while (!(__r % __i)) __r /= __i;
+  }
+  if (__r != 1) *__q++ = __r;
 
-      int_type __p[16] = {2}, *__t = __p + 1;
+  for (int_type __r = 1; __r != __mod; ++__r) {
+    auto __cnt = 0;
+    for (__q = __p; *__q; ++__q) {
+      int_type __w = 1;
+      for (int_type __e = __tot / *__q, __x = __r; __e;
+           __e >>= 1, (__x *= __x) %= __mod)
+        if (__e & 1) (__w *= __x) %= __mod;
+      if (__w == 1 && ++__cnt > 1) break;
+    }
+    if (__cnt == 1) return __r;
+  }
 
-      for (int_type __i = 3; __i <= __m / __i; __i += 2) {
-        if (__m % __i) continue;
-        *__t++ = __i;
-        while (!(__m % __i)) __m /= __i;
-      }
-      if (__m != 1) *__t++ = __m;
-
-      int_type __r = 1;
-      for (bool __yet = true; __yet; ++__r) {
-        __yet = false;
-        for (auto __i = __p; __i != __t; ++__i) {
-          int_type __w = 1, __x = __r;
-          for (auto e = (Mod - 1) / *__i; e; e >>= 1, (__x *= __x) %= Mod)
-            if (e & 1) (__w *= __x) %= Mod;
-          if (__w == 1) {
-            __yet = true;
-            break;
-          }
-        }
-      }
-      return static_cast<result_type>(--__r);
-    }();
+  return 0;
+};
 
 }  // namespace workspace
+
+#endif
