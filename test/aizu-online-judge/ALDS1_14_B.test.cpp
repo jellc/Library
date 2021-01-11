641c53fe5d0c9da1e5f6eea0e872707383714232
jell <euler31415@icloud.com>
2021-01-11 15:46:26 +0900

    fix

--- test/aizu-online-judge/ALDS1_14_B.test.cpp
+++ test/aizu-online-judge/ALDS1_14_B.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_14_B"
 
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 
 #include "src/string/kmp.hpp"
@@ -11,6 +12,11 @@ int main() {
   std::string t, p;
   std::cin >> t >> p;
   auto b = mp_algorithm(p + '$' + t);
-  for (size_t i = 0, j = p.size() * 2; j < b.size(); ++i, ++j)
-    if (b[j + 1] == p.size()) std::cout << i << "\n";
+  auto sb = kmp_algorithm(p + '$' + t);
+  for (size_t i = 0, j = p.size() * 2 + 1; j < b.size(); ++i, ++j)
+    if (b[j] == p.size()) {
+      assert(sb[j] == p.size());
+      std::cout << i << "\n";
+    } else
+      assert(sb[j] != p.size());
 }
