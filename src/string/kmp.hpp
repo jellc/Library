779740edce26ab920bb9fc4e24119016376a1dda
jell <euler31415@icloud.com>
2021-01-11 15:54:41 +0900

    fix

--- src/string/kmp.hpp
+++ src/string/kmp.hpp
@@ -16,7 +16,7 @@ namespace workspace {
  */
 template <class _Str> std::vector<size_t> mp_algorithm(_Str const& __s) {
   std::vector<size_t> __b(std::size(__s) + 1);
-  for (size_t __p{__b[0] = -1}, __q{}; __q != __b.size(); __b[++__q] = ++__p)
+  for (size_t __p{__b[0] = -1}, __q{}; __q != __s.size(); __b[++__q] = ++__p)
     while (~__p && __s[__p] != __s[__q]) __p = __b[__p];
   return __b;
 }
