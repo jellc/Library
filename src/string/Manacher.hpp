78c747de76b0c9609cbca8391b213d204ac146d8
jell <euler31415@icloud.com>
2021-01-15 02:34:06 +0900

    update

--- src/string/Manacher.hpp
+++ src/string/Manacher.hpp
@@ -1,75 +1,33 @@
 #pragma once
 
-/*
+/**
  * @file Manacher.hpp
  * @brief Manacher's Algorithm
  */
 
-#include <iostream>
-#include <string>
 #include <vector>
 
 namespace workspace {
 
-/*
- * @class Manacher
+/**
  * @brief Manacher's Algorithm.
- * @tparam str_type the type of string
+ *
+ * @param __s String
+ * @return Radiuses of the longest palindromic substring with each fixed center.
  */
-template <class str_type = std::string> class Manacher {
-  const size_t len;
-  std::vector<size_t> rad;
-
- public:
-  Manacher(const str_type &str) : len(str.size()), rad(str.size()) {
-    for (size_t i = 0, c = 0; i != len; ++i) {
-      int l = c * 2 - i;
-      if (l >= 0 && c + rad[c] > i + rad[l])
-        rad[i] = rad[l];
-      else {
-        size_t j = c + rad[c] - i;
-        while (i >= j && i + j != len && str[i - j] == str[i + j]) ++j;
-        rad[c = i] = j;
-      }
+template <class _Str> std::vector<size_t> Manacher(_Str const &__s) {
+  const auto __n = std::size(__s);
+  std::vector<size_t> __r(__n);
+  for (size_t __i = 0, __c = 0; __i != __n; ++__i)
+    if (auto __j = __c * 2 - __i; __j < __n && __c + __r[__c] > __i + __r[__j])
+      __r[__i] = __r[__j];
+    else {
+      __j = __c + __r[__c] - __i;
+      while (__i >= __j && __i + __j != __n && __s[__i - __j] == __s[__i + __j])
+        ++__j;
+      __r[__c = __i] = __j;
     }
-  }
-
-  std::vector<size_t>::const_iterator begin() const { return rad.begin(); }
-
-  std::vector<size_t>::const_iterator end() const { return rad.end(); }
-
-  /*
-   * @fn size
-   * @return length of the string
-   */
-  size_t size() const { return rad.size(); }
-
-  /*
-   * @fn operator[]
-   * @param i index
-   * @return the maximum radius L s.t. the substring (i - L, i + L) is
-   * palindrome
-   */
-  size_t operator[](size_t i) const { return rad[i]; }
-
-  /*
-   * @fn operator<<
-   * @brief print a Manacher object.
-   * @param os ostream
-   * @param manacher a Manacher object
-   * @return os
-   */
-  friend std::ostream &operator<<(std::ostream &os, const Manacher &manacher) {
-    bool is_front = true;
-    for (size_t r : manacher.rad) {
-      if (is_front)
-        is_front = false;
-      else
-        os << " ";
-      os << r;
-    }
-    return os;
-  }
-};
+  return __r;
+}
 
 }  // namespace workspace
