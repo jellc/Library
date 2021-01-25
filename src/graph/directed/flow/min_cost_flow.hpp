8e57651e6d813fb0135d53295910f9366df9aad7
jell <euler31415@icloud.com>
2021-01-25 15:58:25 +0900

    capacity scaling

--- src/graph/directed/flow/min_cost_flow.hpp
+++ src/graph/directed/flow/min_cost_flow.hpp
@@ -18,13 +18,12 @@
 namespace workspace {
 
 /**
- * @brief Successive Shortest Path Algorithm.
+ * @brief Capacity Scaling Algorithm.
  *
  * @tparam Cap Capacity type
  * @tparam Cost Cost type
- * @tparam Density_tag Whether the graph is dense
  */
-template <class Cap, class Cost = Cap, bool Density_tag = false>
+template <class Cap, class Cost = Cap>
 class min_cost_flow : public flow_graph<Cap, Cost> {
   using base = flow_graph<Cap, Cost>;
   using edge_impl = typename base::edge_impl;
@@ -105,7 +104,7 @@ class min_cost_flow : public flow_graph<Cap, Cost> {
   }
 
   /**
-   * @return Balance of the graph.
+   * @return Balance.
    */
   const auto &balance() const { return b; }
 
@@ -116,7 +115,7 @@ class min_cost_flow : public flow_graph<Cap, Cost> {
   Cap balance(size_type node) const { return b[node]; }
 
   /**
-   * @return Potential of the graph.
+   * @return Potential. The dual solution.
    */
   const auto &potential() const { return p; }
 
@@ -132,41 +131,52 @@ class min_cost_flow : public flow_graph<Cap, Cost> {
   Cost cost() const { return current; }
 
   /**
-   * @brief Run Successive Shortest Path Algorithm.
+   * @brief Run Capacity Scaling Algorithm.
    *
    * @return Whether a balanced flow exists.
    */
   bool run() {
-    // Saturate negative edges.
+    p.resize(b.size());
+
+    Cap delta = 0;
     for (auto &&__adj : base::graph)
-      for (auto &&__e : __adj)
-        if (__e.cost < static_cast<Cost>(0) && static_cast<Cap>(0) < __e.cap) {
-          b[__e.src] -= __e.cap;
-          b[__e.dst] += __e.cap;
-          __e.push(__e.cap);
-        }
+      for (auto &&__e : __adj) delta = std::max(delta, __e.cap);
+    if (delta == static_cast<Cap>(0))
+      return std::none_of(b.begin(), b.end(),
+                          [](Cap __x) { return __x != static_cast<Cap>(0); });
 
-    sources.clear();
-    sinks.clear();
-    for (size_type __i = 0; __i != b.size(); ++__i)
-      if (b[__i] != static_cast<Cap>(0))
-        (static_cast<Cap>(0) < b[__i] ? sources : sinks).emplace_back(__i);
+    parent.resize(b.size());
 
-    p.resize(size());
-    parent.resize(size());
+    while (static_cast<Cap>(0) < delta) {
+      delta /= 2;
 
-    // Primal-Dual
-    while (dual()) {
-      primal();
-      sources.erase(std::remove_if(sources.begin(), sources.end(),
-                                   [&](auto __v) {
-                                     return b[__v] == static_cast<Cap>(0);
-                                   }),
-                    sources.end());
-      sinks.erase(std::remove_if(
-                      sinks.begin(), sinks.end(),
-                      [&](auto __v) { return b[__v] == static_cast<Cap>(0); }),
-                  sinks.end());
+      for (auto &&__adj : base::graph)
+        for (auto &&__e : __adj)
+          if (delta < __e.cap && __e.cost < p[__e.dst] - p[__e.src]) {
+            b[__e.src] -= __e.cap;
+            b[__e.dst] += __e.cap;
+            __e.push(__e.cap);
+          }
+
+      sources.clear();
+      sinks.clear();
+      for (size_type __v = 0; __v != b.size(); ++__v)
+        if (delta < b[__v])
+          sources.emplace_back(__v);
+        else if (b[__v] < -delta)
+          sinks.emplace_back(__v);
+
+      while (dual(delta)) {
+        primal(delta);
+        sources.erase(
+            std::remove_if(sources.begin(), sources.end(),
+                           [&](auto __v) { return !(delta < b[__v]); }),
+            sources.end());
+        sinks.erase(
+            std::remove_if(sinks.begin(), sinks.end(),
+                           [&](auto __v) { return !(b[__v] < -delta); }),
+            sinks.end());
+      }
     }
 
     current = 0;
@@ -179,7 +189,7 @@ class min_cost_flow : public flow_graph<Cap, Cost> {
 
  protected:
   // Cost of flow.
-  Cost current;
+  Cost current{};
 
   // Balance
   std::vector<Cap> b;
@@ -191,116 +201,76 @@ class min_cost_flow : public flow_graph<Cap, Cost> {
   std::vector<size_type> sources, sinks;
 
   // Augment along the dual solution.
-  void primal() {
+  void primal(Cap delta) {
     for (auto __t : sinks)
       if (parent[__t]) {
         auto __f = -b[__t];
         auto __s = __t;
         while (parent[__s])
           __f = std::min(__f, parent[__s]->cap), __s = parent[__s]->src;
-        if (static_cast<Cap>(0) < b[__s]) {
+        if (delta < b[__s]) {
           __f = std::min(__f, b[__s]);
           b[__s] -= __f;
           b[__t] += __f;
           for (auto *__p = parent[__t]; __p; __p = parent[__p->src]) {
             __p->push(__f);
-            if (__p->cap == static_cast<Cap>(0)) parent[__p->dst] = nullptr;
+            parent[__p->dst] = nullptr;
           }
         }
       }
   }
 
   // Improve the dual solution.
-  bool dual() {
+  bool dual(Cap delta) {
     std::fill(parent.begin(), parent.end(), nullptr);
-
-    constexpr Cost infty = std::numeric_limits<Cost>::max();
-    std::vector<Cost> __nx(size(), infty);
     size_type reachable = 0;
 
-    if constexpr (Density_tag) {  // O(V^2)
-      constexpr static size_type nil = -1;
-      std::vector<bool> used(size());
+    struct state {
+      size_type __v;
+      Cost __d;
+      state(size_type __v, Cost __d) : __v(__v), __d(__d) {}
+      bool operator<(const state &__x) const { return __x.__d < __d; }
+    };
 
-      for (auto __s : sources) {
-        used[__s] = true;
-        __nx[__s] = 0;
-        for (auto &__e : base::graph[__s])
-          if (static_cast<Cap>(0) < __e.cap && __e.cost < __nx[__e.dst])
-            __nx[__e.dst] = __e.cost, parent[__e.dst] = &__e;
-      }
+    std::priority_queue<state> __q;
+    decltype(p) __nx(p.size(), std::numeric_limits<Cost>::max());
+    Cost __ld = 0;
 
-      while (true) {
-        size_type __v{nil};
-        Cost __sd{infty};
+    for (auto __v : sources) {
+      __nx[__v] = p[__v];
+      __q.emplace(__v, 0);
+    }
 
-        for (size_type __i{}; __i != size(); ++__i) {
-          if (used[__i] || __nx[__i] == infty) continue;
-          if (Cost __d = __nx[__i] - p[__i]; __d < __sd) __sd = __d, __v = __i;
+    while (!__q.empty()) {
+      auto [__v, __d] = __q.top();
+      __q.pop();
+      if (__d + p[__v] != __nx[__v]) continue;
+      __ld = __d;
+      if (b[__v] < -delta && ++reachable == sinks.size()) break;
+      for (auto &__e : base::graph[__v])
+        if (delta < __e.cap && (__d = __nx[__v] + __e.cost) < __nx[__e.dst]) {
+          __q.emplace(__e.dst, (__nx[__e.dst] = __d) - p[__e.dst]);
+          parent[__e.dst] = &__e;
         }
-
-        if (__v == nil) break;
-        used[__v] = true;
-        if (b[__v] < static_cast<Cap>(0))
-          if (++reachable == sinks.size()) break;
-
-        for (auto &__e : base::graph[__v])
-          if (Cost __d = __nx[__v] + __e.cost;
-              static_cast<Cap>(0) < __e.cap && __d < __nx[__e.dst]) {
-            __nx[__e.dst] = __d;
-            parent[__e.dst] = &__e;
-          }
-      }
     }
 
-    else {  // O((V + E)logV)
-      struct state {
-        size_type __v;
-        Cost __d;
-        state(size_type __v, Cost __d) : __v(__v), __d(__d) {}
-        bool operator<(const state &__x) const { return __x.__d < __d; }
-      };
+    for (size_type __v = 0; __v != p.size(); ++__v)
+      p[__v] = std::min(__nx[__v], p[__v] += __ld);
 
-      std::priority_queue<state> __q;
-      for (auto __v : sources) {
-        __nx[__v] = 0;
-        for (auto &__e : base::graph[__v])
-          if (static_cast<Cap>(0) < __e.cap && __e.cost < __nx[__e.dst])
-            __q.emplace(__e.dst, (__nx[__e.dst] = __e.cost) - p[__e.dst]),
-                parent[__e.dst] = &__e;
-      }
-
-      while (!__q.empty()) {
-        auto [__v, __d] = __q.top();
-        __q.pop();
-        if (__d + p[__v] != __nx[__v]) continue;
-        if (b[__v] < static_cast<Cap>(0))
-          if (++reachable == sinks.size()) break;
-        for (auto &__e : base::graph[__v])
-          if (auto __d = __nx[__v] + __e.cost;
-              static_cast<Cap>(0) < __e.cap && __d < __nx[__e.dst]) {
-            __q.emplace(__e.dst, (__nx[__e.dst] = __d) - p[__e.dst]);
-            parent[__e.dst] = &__e;
-          }
-      }
-    }
-
-    if (reachable) p = std::move(__nx);
     return reachable;
   }
 
 };  // namespace workspace
 
 /**
- * @brief Successive Shortest Path Algorithm.
+ * @brief Capacity Scaling Algorithm.
  *
  * @tparam Cap Capacity type
  * @tparam Gain Gain type
- * @tparam Density_tag Whether the graph is dense
  */
-template <class Cap, class Gain = Cap, bool Density_tag = false>
-class max_gain_flow : public min_cost_flow<Cap, Gain, Density_tag> {
-  using base = min_cost_flow<Cap, Gain, Density_tag>;
+template <class Cap, class Gain = Cap>
+class max_gain_flow : public min_cost_flow<Cap, Gain> {
+  using base = min_cost_flow<Cap, Gain>;
   using base::cost;
 
  public:
