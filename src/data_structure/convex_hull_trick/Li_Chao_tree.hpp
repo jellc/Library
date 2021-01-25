78091bf3e88938e2be19b75014aba87e040f88f4
jell <euler31415@icloud.com>
2021-01-25 22:14:35 +0900

    fix

--- src/data_structure/convex_hull_trick/Li_Chao_tree.hpp
+++ src/data_structure/convex_hull_trick/Li_Chao_tree.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-/*
+/**
  * @file Li_Chao_tree.hpp
  * @brief Li-Chao Tree
  */
@@ -10,90 +10,86 @@
 
 namespace workspace {
 
-/*
- * @struct Li_Chao_tree
- * @tparam T Ordered ring.
+/**
+ * @brief Li-Chao Tree
+ *
+ * @tparam F Ordered Field.
  * @tparam Comp Comparison function object type
  * @tparam Infinity Identity element for 'Comp'.
  * @tparam Eps Error tolerance
  */
-template <class T, class Comp = std::less<T>,
-          T Infinity = std::numeric_limits<T>::max() / 2, T Eps = 1>
+template <class F, class Comp = std::less<F>,
+          F Infinity = std::numeric_limits<F>::max() / 2, F Eps = 1>
 struct Li_Chao_tree {
-  constexpr static T infinity = Infinity;
-  constexpr static T eps = Eps;
+  constexpr static F infinity = Infinity;
+  constexpr static F eps = Eps;
 
   struct line {
-    T slope = 0, intercept = Infinity;
-    T eval(const T x) const { return slope * x + intercept; }
+    F slope = 0, intercept = Infinity;
+    F eval(F x) const { return slope * x + intercept; }
   };
 
-  /*
+  /**
    * @param begin Lower bound of domain
    * @param end Upper bound of domain
    * @param comp Comparison function object
    */
-  Li_Chao_tree(const T begin, const T end, Comp comp = Comp())
+  Li_Chao_tree(const F begin, const F end, Comp comp = Comp())
       : begin(begin), end(end), comp(comp) {}
 
   ~Li_Chao_tree() { delete root; }
 
   bool empty() const { return !root; }
 
-  /*
-   * @fn insert
+  /**
    * @brief Insert a line.
    * @param ln Line
    */
   void insert(line const &ln) { root = insert(root, begin, end, ln); }
 
-  /*
-   * @fn insert
+  /**
    * @brief Insert a line.
    * @param slope Slope
    * @param intercept Intercept
    */
-  void insert(const T slope, const T intercept) {
+  void insert(const F slope, const F intercept) {
     insert(line{slope, intercept});
   }
 
-  /*
-   * @fn insert
+  /**
    * @brief Insert a segment.
    * @param seg Segment
    * @param __begin Left end, inclusive
    * @param __end Right end, exclusive
    */
-  void insert(line const &seg, const T __begin, const T __end) {
+  void insert(line const &seg, const F __begin, const F __end) {
     if (__begin < __end) root = insert(root, begin, end, seg, __begin, __end);
   }
 
-  /*
-   * @fn insert
+  /**
    * @brief Insert a segment.
    * @param slope Slope
    * @param intercept Intercept
    * @param __begin Left end, inclusive
    * @param __end Right end, exclusive
    */
-  void insert(const T slope, const T intercept, const T __begin,
-              const T __end) {
+  void insert(const F slope, const F intercept, const F __begin,
+              const F __end) {
     insert(line{slope, intercept}, __begin, __end);
   }
 
-  /*
-   * @fn eval
+  /**
    * @param x Position
    * @return The optimum at given position.
    */
-  T eval(const T x) const {
+  F eval(const F x) const {
     node *p = root;
-    T l = begin, r = end, opt = Infinity;
+    F l = begin, r = end, opt = Infinity;
     while (p) {
-      const T nval = p->__line.eval(x);
+      const F nval = p->__line.eval(x);
       if (comp(nval, opt)) opt = nval;
       if (r - l <= Eps) break;
-      const T mid = (l + r) / 2;
+      const F mid = (l + r) / 2;
       if (x < mid) {
         p = p->__left;
         r = mid;
@@ -105,20 +101,19 @@ struct Li_Chao_tree {
     return opt;
   }
 
-  /*
-   * @fn argmin
+  /**
    * @param x Position
    * @return The line achieving the optimum at given position.
    */
-  line get(const T x) const {
+  line get(const F x) const {
     assert(!empty());
     node *p = root, *arg = nullptr;
-    T l = begin, r = end, opt = Infinity;
+    F l = begin, r = end, opt = Infinity;
     while (p) {
-      const T nval = p->__line.eval(x);
+      const F nval = p->__line.eval(x);
       if (comp(nval, opt)) opt = nval, arg = p;
       if (r - l <= Eps) break;
-      const T mid = (l + r) / 2;
+      const F mid = (l + r) / 2;
       if (x < mid) {
         p = p->__left;
         r = mid;
@@ -134,19 +129,71 @@ struct Li_Chao_tree {
   struct node {
     line __line;
     node *__left = nullptr, *__right = nullptr;
-    node(node const &) = delete;
-    node &operator=(node const &) = delete;
+
+    node(line const &__l = line{}) : __line(__l) {}
+
+    node(node const &__x) : __line(__x.__line) {
+      if (__x.__left) __left = new node{*__x.__left};
+      if (__x.__right) __right = new node{*__x.__right};
+    }
+
+    node(node &&__x)
+        : __line(__x.__line), __left(__x.__left), __right(__x.__right) {
+      __x.__left = __x.__right = nullptr;
+    }
+
+    node &operator=(node const &__x) {
+      __line = __x.__line;
+      delete __left;
+      delete __right;
+      if (__x.__left) __left = new node{*__x.__left};
+      if (__x.__right) __right = new node{*__x.__right};
+    }
+
+    node &operator=(node &&__x) {
+      __line = __x.__line;
+      std::swap(__left, __x.__left);
+      std::swap(__right, __x.__right);
+    }
+
     ~node() {
       delete __left;
       delete __right;
     }
   };
 
-  const T begin, end;
+  F begin, end;
   Comp comp;
   node *root = nullptr;
 
-  node *insert(node *const p, const T l, const T r, line const &ln) {
+ public:
+  Li_Chao_tree(const Li_Chao_tree &__x)
+      : begin(__x.begin), end(__x.end), comp(__x.comp) {
+    if (__x.root) root = new node{*__x.root};
+  }
+
+  Li_Chao_tree(Li_Chao_tree &&__x)
+      : begin(__x.begin), end(__x.end), comp(__x.comp), root(__x.root) {
+    __x.root = nullptr;
+  }
+
+  Li_Chao_tree &operator=(const Li_Chao_tree &__x) {
+    begin = __x.begin;
+    end = __x.end;
+    comp = __x.comp;
+    if (__x.root) root = new node{*__x.root};
+  }
+
+  Li_Chao_tree &operator=(Li_Chao_tree &&__x) {
+    begin = __x.begin;
+    end = __x.end;
+    comp = __x.comp;
+    root = __x.root;
+    __x.root = nullptr;
+  }
+
+ protected:
+  node *insert(node *const p, const F l, const F r, line const &ln) {
     if (!p) return new node{ln};
     line &cur = p->__line;
     const bool left_low = comp(ln.eval(l), cur.eval(l));
@@ -154,7 +201,7 @@ struct Li_Chao_tree {
       if (left_low) cur = ln;
       return p;
     }
-    const T mid = (l + r) / 2;
+    const F mid = (l + r) / 2;
     if (comp(ln.eval(mid), cur.eval(mid))) {
       if (left_low)
         p->__right = insert(p->__right, mid, r, cur);
@@ -170,8 +217,8 @@ struct Li_Chao_tree {
     return p;
   }
 
-  node *insert(node *p, const T l, const T r, line const &seg, const T s,
-               const T t) {
+  node *insert(node *p, const F l, const F r, line const &seg, const F s,
+               const F t) {
     if (s < l + Eps && r < t + Eps) return insert(p, l, r, seg);
     if (l < t && s < r) {
       if (!p) p = new node{};
