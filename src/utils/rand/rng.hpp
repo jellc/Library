52f442cff0f306d657c382bcffd8e7464a5a429f
jell <euler31415@icloud.com>
2021-02-24 23:27:21 +0900

    ctor

--- src/utils/rand/rng.hpp
+++ src/utils/rand/rng.hpp
@@ -9,24 +9,29 @@
 
 namespace workspace {
 
-template <typename Arithmetic>
-using uniform_distribution =
-    typename std::conditional<std::is_integral<Arithmetic>::value,
-                              std::uniform_int_distribution<Arithmetic>,
-                              std::uniform_real_distribution<Arithmetic>>::type;
+template <typename _Arithmetic>
+using uniform_distribution = typename std::conditional<
+    std::is_integral<_Arithmetic>::value,
+    std::uniform_int_distribution<_Arithmetic>,
+    std::uniform_real_distribution<_Arithmetic>>::type;
 
-template <typename Arithmetic>
-class random_number_generator : uniform_distribution<Arithmetic> {
-  using base = uniform_distribution<Arithmetic>;
+template <typename _Arithmetic>
+class random_number_generator : uniform_distribution<_Arithmetic> {
+  using base = uniform_distribution<_Arithmetic>;
 
   std::mt19937 engine;
 
  public:
-  template <class... Args>
-  random_number_generator(Args&&... args)
-      : base(args...), engine(std::random_device{}()) {}
+  random_number_generator(_Arithmetic __min, _Arithmetic __max)
+      : base(__min, __max), engine(std::random_device{}()) {}
 
-  auto operator()() { return base::operator()(engine); }
+  random_number_generator(_Arithmetic __max = 1)
+      : base(_Arithmetic(0), __max), engine(std::random_device{}()) {}
+
+  random_number_generator(typename base::param_type const& __param)
+      : base(__param) {}
+
+  decltype(auto) operator()() noexcept { return base::operator()(engine); }
 };
 
 }  // namespace workspace
