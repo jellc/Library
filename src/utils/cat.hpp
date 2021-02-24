33db809464761af172181f5817ac250d5cddea57
jell <euler31415@icloud.com>
2021-02-25 03:01:05 +0900

    modify

--- src/utils/cat.hpp
+++ src/utils/cat.hpp
@@ -9,10 +9,35 @@
 
 namespace workspace {
 
-template <class C1, class C2>
-constexpr C1 &&cat(C1 &&__c1, C2 const &__c2) noexcept {
-  __c1.insert(__c1.end(), std::begin(__c2), std::end(__c2));
-  return __c1;
+/**
+ * @brief Concatenate two sequences.
+ *
+ * @param __c1
+ * @param __c2
+ * @return Concatenated sequence.
+ */
+template <class _C1, class _C2>
+constexpr decltype(auto) cat(_C1 &&__c1, _C2 &&__c2) noexcept {
+  auto __c = std::forward<_C1>(__c1);
+
+  if constexpr (std::is_rvalue_reference<decltype(__c2)>::value)
+    __c.insert(std::end(__c), std::move_iterator(std::begin(__c2)),
+               std::move_iterator(std::end(__c2)));
+
+  else
+    __c.insert(std::end(__c), std::cbegin(__c2), std::cend(__c2));
+
+  return __c;
+}
+
+/**
+ * @return Concatenated sequence.
+ */
+template <class _C1, class _C2, class... _Args>
+constexpr decltype(auto) cat(_C1 &&__c1, _C2 &&__c2,
+                             _Args &&...__args) noexcept {
+  return cat(cat(std::forward<_C1>(__c1), std::forward<_C2>(__c2)),
+             std::forward<_Args>(__args)...);
 }
 
 }  // namespace workspace
