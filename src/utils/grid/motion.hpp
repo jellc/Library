0f5a772103fd4cfa1ad897b28904782f0b009f34
jell <euler31415@icloud.com>
2021-03-30 00:48:08 +0900

    modify move

--- src/utils/grid/motion.hpp
+++ src/utils/grid/motion.hpp
@@ -14,18 +14,20 @@ namespace workspace {
  *
  * @param __grid
  */
-template <class _Grid, typename = decltype(std::declval<std::decay_t< _Grid>>()[0].resize(0))>
+template <class _Grid,
+          typename = decltype(std::declval<std::decay_t<_Grid>>()[0].resize(0))>
 constexpr decltype(auto) transpose(_Grid &&__grid) {
   auto __h = std::size(__grid), __w = std::size(__grid[0]);
 
   std::decay_t<_Grid> __t(__w);
   for (auto &&__r : __t) __r.resize(__h);
 
-  using reference = decltype(__grid[0][0]);
-
   for (size_t __i = 0; __i != __h; ++__i)
     for (size_t __j = 0; __j != __w; ++__j)
-      __t[__j][__i] = std::forward<reference>(__grid[__i][__j]);
+      if constexpr (std::is_rvalue_reference<decltype(__grid)>::value)
+        __t[__j][__i] = std::move(__grid[__i][__j]);
+      else
+        __t[__j][__i] = __grid[__i][__j];
 
   return __t;
 }
@@ -100,34 +102,18 @@ constexpr decltype(auto) transpose(
  * @param __grid
  * @return
  */
-template <class _Grid> decltype(auto) roll_ccw(const _Grid &__grid) {
-  auto __t = transpose(__grid);
-  std::reverse(std::begin(__t), std::end(__t));
-  return __t;
-}
-
-/**
- * @brief Roll the grid counter-clockwise.
- *
- * @param __grid
- * @return
- */
 template <class _Grid> decltype(auto) roll_ccw(_Grid &&__grid) {
-  auto __t = transpose(std::move(__grid));
-  std::reverse(std::begin(__t), std::end(__t));
-  return __t;
-}
+  if constexpr (std::is_rvalue_reference<decltype(__grid)>::value) {
+    auto __t = transpose(std::move(__grid));
+    std::reverse(std::begin(__t), std::end(__t));
+    return __t;
+  }
 
-/**
- * @brief Roll the grid clockwise.
- *
- * @param __grid
- * @return
- */
-template <class _Grid> decltype(auto) roll_cw(const _Grid &__grid) {
-  auto __t = transpose(__grid);
-  for (auto &&__r : __t) std::reverse(std::begin(__r), std::end(__r));
-  return __t;
+  else {
+    auto __t = transpose(__grid);
+    std::reverse(std::begin(__t), std::end(__t));
+    return __t;
+  }
 }
 
 /**
@@ -137,8 +123,16 @@ template <class _Grid> decltype(auto) roll_cw(const _Grid &__grid) {
  * @return
  */
 template <class _Grid> decltype(auto) roll_cw(_Grid &&__grid) {
-  std::reverse(std::begin(__grid), std::end(__grid));
-  return transpose(std::move(__grid));
+  if constexpr (std::is_rvalue_reference<decltype(__grid)>::value) {
+    std::reverse(std::begin(__grid), std::end(__grid));
+    return transpose(std::move(__grid));
+  }
+
+  else {
+    auto __t = transpose(__grid);
+    for (auto &&__r : __t) std::reverse(std::begin(__r), std::end(__r));
+    return __t;
+  }
 }
 
 }  // namespace workspace
