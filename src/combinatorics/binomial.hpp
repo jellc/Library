a1971b68067dd30baf0bb62ea3f0ec5b28decb7b
jell <euler31415@icloud.com>
2021-04-28 17:04:31 +0900

    exact binom

--- src/combinatorics/binomial.hpp
+++ src/combinatorics/binomial.hpp
@@ -3,9 +3,6 @@
 /**
  * @file binomial.hpp
  * @brief Binomial Coefficient
- * @date 2021-01-15
- *
- *
  */
 
 #include "../modular/modint.hpp"
@@ -13,14 +10,46 @@
 
 namespace workspace {
 
+namespace _binom_impl {
+
+struct _binom_table {
+  constexpr static int size = 132;
+  __uint128_t __b[size][size]{1};
+
+  constexpr _binom_table() noexcept {
+    for (int __i = 1; __i != size; ++__i)
+      for (int __j = 0; __j != __i; ++__j)
+        __b[__i][__j] += __b[__i - 1][__j],
+            __b[__i][__j + 1] += __b[__i - 1][__j];
+  }
+
+  constexpr auto operator()(int __x, int __y) const noexcept {
+    return __x < 0 || __x < __y ? 0 : (assert(__x < size), __b[__x][__y]);
+  }
+};
+
+constexpr _binom_table table;
+
+}  // namespace _binom_impl
+
 /**
- * @brief Binomial coefficient for integer args.
+ * @brief Binomial coefficient for integer args. Be careful with overflow.
  */
-template <class Tp> Tp binomial(int_fast32_t __x, int_fast32_t __y) {
-  if (!__y) return 1;
+template <class _Tp> constexpr _Tp binomial(int32_t __x, int32_t __y) {
+  if constexpr (is_integral_ext<_Tp>::value)
+    return _binom_impl::table(__x, __y);
+
   if (__y < 0 || __x < __y) return 0;
-  return factorial<Tp>(__x) * factorial_inverse<Tp>(__y) *
-         factorial_inverse<Tp>(__x - __y);
+
+  return factorial<_Tp>(__x) * factorial_inverse<_Tp>(__y) *
+         factorial_inverse<_Tp>(__x - __y);
+}
+
+/**
+ * @brief Catalan number.
+ */
+template <class _Tp> constexpr _Tp catalan(int32_t __x) {
+  return binomial<_Tp>(__x << 1, __x) - binomial<_Tp>(__x << 1, __x + 1);
 }
 
 }  // namespace workspace
