01455eccfc560265a8e933dd0bde9a3715d5a54d
jell <euler31415@icloud.com>
2021-05-16 13:37:12 +0900

    once

---
+++ src/sys/call_once.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+/**
+ * @file call_once.hpp
+ * @brief Call Once
+ */
+
+#include <unordered_set>
+
+namespace workspace {
+
+/**
+ * @brief Call once.
+ */
+template <class _F> void call_once(_F &&__f) {
+  static std::unordered_set<void *> __called;
+  if (__called.count(std::addressof(__f))) return;
+  __called.emplace(std::addressof(__f));
+  __f();
+}
+
+}  // namespace workspace
