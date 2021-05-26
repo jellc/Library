f5a058f83b24047d8dfe3c62d402a11a1daf21aa
jell <euler31415@icloud.com>
2021-05-27 01:59:50 +0900

    .

--- src/string/kmp.hpp
+++ src/string/kmp.hpp
@@ -1,7 +1,8 @@
+#pragma once
+
 /**
  * @file kmp.hpp
  * @brief Knuth-Morris-Pratt Algorithm
- * @date 2021-01-08
  */
 
 #include <vector>
@@ -12,10 +13,10 @@ namespace workspace {
  * @brief Morris-Pratt algorithm.
  *
  * @param __s String
- * @return The border array of given string.
+ * @return Border array of given string.
  */
-template <class _Str> std::vector<size_t> mp_algorithm(_Str const& __s) {
-  std::vector<size_t> __b(std::size(__s) + 1);
+template <class _String> auto mp_algorithm(const _String& __s) {
+  std::vector<size_t> __b(__s.size() + 1);
   for (size_t __p{__b[0] = -1}, __q{}; __q != __s.size(); __b[++__q] = ++__p)
     while (~__p && __s[__p] != __s[__q]) __p = __b[__p];
   return __b;
@@ -25,9 +26,9 @@ template <class _Str> std::vector<size_t> mp_algorithm(_Str const& __s) {
  * @brief Knuth-Morris-Pratt algorithm.
  *
  * @param __s String
- * @return The strong-border array of given string.
+ * @return Strong-border array of given string.
  */
-template <class _Str> std::vector<size_t> kmp_algorithm(_Str const& __s) {
+template <class _String> auto kmp_algorithm(const _String& __s) {
   auto __b = mp_algorithm(__s);
   for (size_t __i{1}; __i != __s.size(); ++__i)
     if (__s[__i] == __s[__b[__i]]) __b[__i] = __b[__b[__i]];
