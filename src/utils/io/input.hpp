59d065f9cd6545c01383a17a348b6450dba920ec
jell <euler31415@icloud.com>
2021-06-20 01:15:55 +0900

    input

---
+++ src/utils/io/input.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+/**
+ * @file input.hpp
+ * @brief Input
+ */
+
+#include "istream.hpp"
+
+namespace workspace {
+
+namespace _input_impl {
+
+template <class _Tp, bool _Is_class = false> class input {
+  _Tp __value;
+
+  template <class _Arg, class... _Args> struct is_same : std::false_type {};
+  template <class _Arg> struct is_same<_Arg, _Arg> : std::true_type {};
+
+ public:
+  operator _Tp &() noexcept { return __value; }
+  operator const _Tp &() const noexcept { return __value; }
+
+  template <class... _Args>
+  input(_Args &&...__args) noexcept : __value(std::forward<_Args>(__args)...) {
+    if _CXX17_CONSTEXPR (!is_same<decltype(*this), _Args...>::value &&
+                         !is_same<_Tp, _Args...>::value)
+      cin >> __value;
+  }
+
+  input &operator=(const _Tp &__x) noexcept { return __value = __x, *this; }
+};
+
+template <class _Tp> class input<_Tp, true> : public _Tp {
+  template <class _Arg, class... _Args> struct is_same : std::false_type {};
+  template <class _Arg> struct is_same<_Arg, _Arg> : std::true_type {};
+
+ public:
+  operator _Tp &() noexcept { return *this; }
+  operator const _Tp &() const noexcept { return *this; }
+
+  template <class... _Args>
+  input(_Args &&...__args) noexcept : _Tp(std::forward<_Args>(__args)...) {
+    if _CXX17_CONSTEXPR (!is_same<decltype(*this), _Args...>::value &&
+                         !is_same<_Tp, _Args...>::value)
+      cin >> *this;
+  }
+
+  input &operator=(const _Tp &__x) noexcept {
+    _Tp::operator=(__x);
+    return *this;
+  }
+};
+
+}  // namespace _input_impl
+
+template <class _Tp>
+using input = _input_impl::input<_Tp, std::is_class<_Tp>::value>;
+
+}  // namespace workspace
