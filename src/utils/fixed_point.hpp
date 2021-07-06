e67f8775bd523d806b55a5b228f99cde879ec503
jell <euler31415@icloud.com>
2021-07-06 17:30:05 +0900

    noexcept

--- src/utils/fixed_point.hpp
+++ src/utils/fixed_point.hpp
@@ -13,25 +13,28 @@ namespace workspace {
  * @brief Fixed Point Combinator.
  */
 template <class _F> class fixed_point {
+  struct _wrapper {
+    _F &__ref;
+
+    template <class... _Args>
+    decltype(auto) operator()(_Args &&...__args) noexcept(
+        noexcept(__ref(*this, std::forward<_Args>(__args)...))) {
+      return __ref(*this, std::forward<_Args>(__args)...);
+    }
+  };
+
+  _F __fn;
+
  public:
   // Construct a new fixed-point object.
   fixed_point(_F __x) noexcept : __fn(__x) {}
 
   // Function call.
-  template <class... _Args> decltype(auto) operator()(_Args &&...__args) {
+  template <class... _Args>
+  decltype(auto) operator()(_Args &&...__args) noexcept(noexcept(_wrapper{
+      __fn}(std::forward<_Args>(__args)...))) {
     return _wrapper{__fn}(std::forward<_Args>(__args)...);
   }
-
- private:
-  _F __fn;
-
-  struct _wrapper {
-    _F &__ref;
-
-    template <class... _Args> decltype(auto) operator()(_Args &&...__args) {
-      return __ref(*this, std::forward<_Args>(__args)...);
-    }
-  };
 };
 
 }  // namespace workspace
