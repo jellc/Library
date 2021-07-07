4c2bf6682a0a3b50b98f1271a03a9de671c4a81a
jell <euler31415@icloud.com>
2021-07-07 20:07:23 +0900

    size

--- src/data_structure/ndvec.hpp
+++ src/data_structure/ndvec.hpp
@@ -7,33 +7,54 @@
 
 #include <vector>
 
+#include "lib/cxx17"
+
 namespace workspace {
 
-template <class _Tp, size_t _Nm>
-class ndvec : public std::vector<ndvec<_Tp, _Nm - 1>> {
+template <class _Tp, size_t _Dim>
+class ndvec : public std::vector<ndvec<_Tp, _Dim - 1>> {
  public:
-  static constexpr auto dimension = _Nm;
-  using container_type = std::vector<ndvec<_Tp, _Nm - 1>>;
+  static constexpr auto dimension = _Dim;
+  using container_type = std::vector<ndvec<_Tp, _Dim - 1>>;
+  using size_type = typename container_type::size_type;
 
-  template <class _Dim, size_t _Size>
-  ndvec(const _Dim (&__dim)[_Size], const _Tp& __x = {})
-      : container_type(__dim[_Size - _Nm], {__dim, __x}) {}
+  ndvec() = default;
+
+  template <class _Size, size_t _Nm>
+  ndvec(const _Size (&__size)[_Nm], const _Tp& __x = {}) noexcept
+      : container_type(__size[_Nm - _Dim], {__size, __x}) {}
+
+  template <size_type _Nm = 0> size_type size() const noexcept {
+    _CXX17_STATIC_ASSERT(_Nm < dimension);
+    if _CXX17_CONSTEXPR (_Nm) {
+      return container_type::operator[](0).template size<_Nm - 1>();
+    }
+    return container_type::size();
+  }
 };
 
 template <class _Tp> class ndvec<_Tp, 1> : public std::vector<_Tp> {
  public:
   static constexpr auto dimension = 1;
   using container_type = std::vector<_Tp>;
+  using size_type = typename container_type::size_type;
 
-  template <class _Dim, size_t _Size>
-  ndvec(const _Dim (&__dim)[_Size], const _Tp& __x = {})
-      : container_type(__dim[_Size - 1], __x) {}
+  ndvec() = default;
+
+  template <class _Size, size_t _Nm>
+  ndvec(const _Size (&__size)[_Nm], const _Tp& __x = {}) noexcept
+      : container_type(__size[_Nm - 1], __x) {}
+
+  template <size_type _Nm = 0> size_type size() const noexcept {
+    _CXX17_STATIC_ASSERT(!_Nm);
+    return container_type::size();
+  }
 };
 
 #if __cplusplus >= 201703L
 
-template <class _Tp, class _Dim, size_t _Nm>
-ndvec(const _Dim (&)[_Nm], const _Tp&) -> ndvec<_Tp, _Nm>;
+template <class _Tp, class _Size, size_t _Dim>
+ndvec(const _Size (&)[_Dim], const _Tp&) -> ndvec<_Tp, _Dim>;
 
 #endif
 
