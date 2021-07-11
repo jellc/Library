ecfefabd936f5dc710121383fb9e953f8b323354
jell <euler31415@icloud.com>
2021-07-11 22:33:28 +0900

    misc

---
+++ src/misc/inversion.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+/**
+ * @file inversion.hpp
+ * @brief Inversion
+ */
+
+#include <algorithm>
+#include <vector>
+
+namespace workspace {
+
+template <class _Iterator>
+auto left_inversion(_Iterator __first, _Iterator __last) noexcept {
+  std::vector<typename std::iterator_traits<_Iterator>::value_type> __v(__first,
+                                                                        __last);
+  std::sort(__v.begin(), __v.end(), greater<void>{});
+  std::vector<size_t> __inv, __sum(__v.size() + 1);
+  while (__first != __last) {
+    auto& __c = __inv.emplace_back();
+    size_t __e = std::distance(
+        __v.begin(),
+        std::lower_bound(__v.begin(), __v.end(), *__first++, greater<void>{}));
+    for (auto __i = ++__e; __i; __i &= __i - 1) __c += __sum[__i];
+    for (; __e < __sum.size(); __e += __e & -__e) ++__sum[__e];
+  }
+  return __inv;
+}
+
+template <class _Iterator>
+auto right_inversion(_Iterator __first, _Iterator __last) noexcept {
+  std::vector<typename std::iterator_traits<_Iterator>::value_type> __v(__first,
+                                                                        __last);
+  std::sort(__v.begin(), __v.end());
+  std::vector<size_t> __inv, __sum(__v.size() + 1);
+  while (__first != __last) {
+    auto& __c = __inv.emplace_back();
+    size_t __e = std::distance(
+        __v.begin(), std::lower_bound(__v.begin(), __v.end(), *--__last));
+    for (auto __i = ++__e; __i; __i &= __i - 1) __c += __sum[__i];
+    for (; __e < __sum.size(); __e += __e & -__e) ++__sum[__e];
+  }
+  std::reverse(__inv.begin(), __inv.end());
+  return __inv;
+}
+
+template <class _Iterator>
+auto inversion(_Iterator __first, _Iterator __last) noexcept {
+  size_t __res = 0;
+  for (auto __x : left_inversion(__first, __last)) __res += __x;
+  return __res;
+}
+
+}  // namespace workspace
---
+++ src/misc/majority_vote.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+/**
+ * @file majority_vote.hpp
+ * @brief Boyer-Moore Majority Vote Algorithm
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace workspace {
+
+/**
+ * @brief Boyer-Moore majority vote algorithm.
+ * @return Elements which appears at least k times in given range.
+ */
+template <class _Iterator>
+auto majority_vote(_Iterator __first, _Iterator __last, size_t __k) {
+  assert(__k);
+
+  using value_type = typename std::iterator_traits<_Iterator>::value_type;
+
+  struct pile {
+    value_type value;
+    size_t count;
+    operator value_type() const { return value; }
+  };
+
+  std::vector<pile> __piles;
+  size_t __max = std::distance(__first, __last) / __k;
+
+  for (auto __it = __first; __it != __last; ++__it) {
+    bool __exist = false;
+
+    for (auto &__x : __piles)
+      if (__x.value == *__it) {
+        ++__x.count, __exist = true;
+        break;
+      }
+
+    if (__exist) continue;
+
+    if (__piles.size() < __max)
+      __piles.push_back({*__it, 1});
+
+    else {
+      for (auto &__x : __piles) __x.count -= 1;
+      __piles.erase(std::remove_if(__piles.begin(), __piles.end(),
+                                   [](const auto &__x) { return !__x.count; }),
+                    __piles.end());
+    }
+  }
+
+  for (auto &&__x : __piles) __x.count = 0;
+
+  while (__first != __last) {
+    for (auto &__x : __piles)
+      if (__x.value == *__first) {
+        ++__x.count;
+        break;
+      }
+    ++__first;
+  }
+
+  __piles.erase(
+      std::remove_if(__piles.begin(), __piles.end(),
+                     [&](const auto &__x) { return __x.count < __k; }),
+      __piles.end());
+
+  return __piles;
+}
+
+}  // namespace workspace
