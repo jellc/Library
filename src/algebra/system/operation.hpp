d9df3edf540a4acb75b956c9f4c9955e9f5f1564
jell <euler31415@icloud.com>
2021-07-11 22:06:06 +0900

    <

--- src/algebra/system/operation.hpp
+++ src/algebra/system/operation.hpp
@@ -111,4 +111,21 @@ struct has_arithmetic<_Tp1, _Tp2, require_binary_plus<_Tp1, _Tp2>,
 template <class _Tp1, class _Tp2 = _Tp1>
 using require_arithmetic = std::enable_if_t<has_arithmetic<_Tp1, _Tp2>::value>;
 
+// Binary `<`
+template <class _Tp, class = void> struct is_comparable : std::false_type {};
+
+template <class _Tp>
+struct is_comparable<_Tp, std::__void_t<decltype(std::declval<const _Tp &>() <
+                                                 std::declval<const _Tp &>())>>
+    : std::true_type {};
+
+template <class _Tp, bool _Default = false> struct try_less : std::less<_Tp> {
+  constexpr bool operator()(const _Tp &__x, const _Tp &__y) noexcept {
+    if _CXX17_CONSTEXPR (is_comparable<_Tp>::value)
+      return std::less<_Tp>::operator()(__x, __y);
+    else
+      return _Default;
+  }
+};
+
 }  // namespace workspace
