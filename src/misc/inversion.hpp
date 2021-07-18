ba471c79a3707e626bfa6c2a4b22024a7084c435
jell <euler31415@icloud.com>
2021-07-18 20:41:29 +0900

    fix

--- src/misc/inversion.hpp
+++ src/misc/inversion.hpp
@@ -14,15 +14,15 @@ template <class _Iterator>
 auto left_inversion(_Iterator __first, _Iterator __last) noexcept {
   std::vector<typename std::iterator_traits<_Iterator>::value_type> __v(__first,
                                                                         __last);
-  std::sort(__v.begin(), __v.end(), greater<void>{});
+  std::sort(__v.begin(), __v.end(), std::greater<void>{});
   std::vector<size_t> __inv, __sum(__v.size() + 1);
   while (__first != __last) {
     auto& __c = __inv.emplace_back();
     size_t __e = std::distance(
-        __v.begin(),
-        std::lower_bound(__v.begin(), __v.end(), *__first++, greater<void>{}));
-    for (auto __i = ++__e; __i; __i &= __i - 1) __c += __sum[__i];
-    for (; __e < __sum.size(); __e += __e & -__e) ++__sum[__e];
+        __v.begin(), std::lower_bound(__v.begin(), __v.end(), *__first++,
+                                      std::greater<void>{}));
+    for (auto __i = __e; __i; __i &= __i - 1) __c += __sum[__i];
+    for (++__e; __e < __sum.size(); __e += __e & -__e) ++__sum[__e];
   }
   return __inv;
 }
@@ -37,8 +37,8 @@ auto right_inversion(_Iterator __first, _Iterator __last) noexcept {
     auto& __c = __inv.emplace_back();
     size_t __e = std::distance(
         __v.begin(), std::lower_bound(__v.begin(), __v.end(), *--__last));
-    for (auto __i = ++__e; __i; __i &= __i - 1) __c += __sum[__i];
-    for (; __e < __sum.size(); __e += __e & -__e) ++__sum[__e];
+    for (auto __i = __e; __i; __i &= __i - 1) __c += __sum[__i];
+    for (++__e; __e < __sum.size(); __e += __e & -__e) ++__sum[__e];
   }
   std::reverse(__inv.begin(), __inv.end());
   return __inv;
