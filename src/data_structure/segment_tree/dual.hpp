3ba304df931b98eda8422f2a791b51dff86ebe5b
jell <euler31415@icloud.com>
2021-07-24 19:56:03 +0900

    partition

--- src/data_structure/segment_tree/dual.hpp
+++ src/data_structure/segment_tree/dual.hpp
@@ -22,21 +22,68 @@ template <class _Tp> class dual_segment_tree {
       : __len(__n),
         __data(2 << (__n > 1 ? 64 - __builtin_clzll(__n - 1) : 0), __x) {}
 
-  template <class _Op> void point(size_type __i, _Op &&__op) {
+  template <class _Op>
+  void point(size_type __i,
+             _Op &&__op) noexcept(noexcept(__op(std::declval<_Tp &>()))) {
     assert(__i < __len);
-    for (__i += __data.size() >> 1; __i; __i >>= 1) __op(__data[__i]);
+    for (__i += size_ext(); __i; __i >>= 1) __op(__data[__i]);
   }
 
-  template <class _Op> void range(size_type __l, size_type __r, _Op &&__op) {
+  template <class _Op>
+  void range(size_type __l, size_type __r,
+             _Op &&__op) noexcept(noexcept(__op(std::declval<_Tp &>()))) {
     assert(__l <= __r), assert(__r <= __len);
-    for (__l += __data.size() >> 1, __r += __data.size() >> 1; __l != __r;
+    for (__l += size_ext(), __r += size_ext(); __l != __r;
          __l >>= 1, __r >>= 1) {
       if (__l & 1) __op(__data[__l++]);
       if (__r & 1) __op(__data[--__r]);
     }
   }
 
+  template <class _Predicate>
+  size_type left_partition(size_type __r, _Predicate &&__pred) noexcept(
+      noexcept(__pred(std::declval<_Tp &>()))) {
+    assert(__r <= __len);
+    __r += size_ext();
+    for (auto __l = size_ext(); __l != __r; __l >>= 1, __r >>= 1)
+      if ((__l & 1) != (__r & 1))
+        if (!__pred(__data[--__r])) return left_partition_subtree(__r, __pred);
+    return 0;
+  }
+
+  template <class _Predicate>
+  size_type right_partition(size_type __l, _Predicate &&__pred) noexcept(
+      noexcept(__pred(std::declval<_Tp &>()))) {
+    assert(__l < __len);
+    __l += size_ext();
+    for (auto __r = __data.size(); __l != __r; __l >>= 1, __r >>= 1)
+      if ((__l & 1) != (__r & 1)) {
+        if (!__pred(__data[__l])) return left_partition_subtree(__l, __pred);
+        ++__l;
+      }
+    return __len;
+  }
+
  private:
+  template <class _Predicate>
+  size_type left_partition_subtree(size_type __i, _Predicate &&__pred) noexcept(
+      noexcept(__pred(std::declval<_Tp &>()))) {
+    while (__i < size_ext())
+      if (!__pred(__data[(__i <<= 1) | 1])) __i |= 1;
+    return ++__i -= size_ext();
+  }
+
+  template <class _Predicate>
+  size_type right_partition_subtree(
+      size_type __i,
+      _Predicate &&__pred) noexcept(noexcept(__pred(std::declval<_Tp &>()))) {
+    while (__i < size_ext())
+      if (__pred(__data[__i <<= 1])) __i |= 1;
+    return std::min(__i -= size_ext(), __len);
+  }
+
+  size_type size_ext() const noexcept { return __data.size() >> 1; }
+
   size_type __len;
   container_type __data;
 };
