710f8c9c0ea0985e22b86b4148aae86c805a0dc4
jell <euler31415@icloud.com>
2021-07-25 13:33:07 +0900

    fix

--- src/utils/io/input.hpp
+++ src/utils/io/input.hpp
@@ -15,8 +15,9 @@ namespace _input_impl {
 template <class _Tp, bool _Is_class = false> class input {
   _Tp __value;
 
-  template <class _Arg, class... _Args> struct is_same : std::false_type {};
-  template <class _Arg> struct is_same<_Arg, _Arg> : std::true_type {};
+  template <class... _Args> struct is_convertible : std::false_type {};
+  template <class _Arg>
+  struct is_convertible<_Arg> : std::is_convertible<_Arg, _Tp> {};
 
  public:
   operator _Tp &() noexcept { return __value; }
@@ -24,17 +25,14 @@ template <class _Tp, bool _Is_class = false> class input {
 
   template <class... _Args>
   input(_Args &&...__args) noexcept : __value(std::forward<_Args>(__args)...) {
-    if _CXX17_CONSTEXPR (!is_same<decltype(*this), _Args...>::value &&
-                         !is_same<_Tp, _Args...>::value)
-      cin >> __value;
+    if _CXX17_CONSTEXPR (not is_convertible<_Args...>::value) cin >> __value;
   }
-
-  input &operator=(const _Tp &__x) noexcept { return __value = __x, *this; }
 };
 
 template <class _Tp> class input<_Tp, true> : public _Tp {
-  template <class _Arg, class... _Args> struct is_same : std::false_type {};
-  template <class _Arg> struct is_same<_Arg, _Arg> : std::true_type {};
+  template <class... _Args> struct is_convertible : std::false_type {};
+  template <class _Arg>
+  struct is_convertible<_Arg> : std::is_convertible<_Arg, _Tp> {};
 
  public:
   operator _Tp &() noexcept { return *this; }
@@ -42,15 +40,11 @@ template <class _Tp> class input<_Tp, true> : public _Tp {
 
   template <class... _Args>
   input(_Args &&...__args) noexcept : _Tp(std::forward<_Args>(__args)...) {
-    if _CXX17_CONSTEXPR (!is_same<decltype(*this), _Args...>::value &&
-                         !is_same<_Tp, _Args...>::value)
-      cin >> *this;
+    if _CXX17_CONSTEXPR (not is_convertible<_Args...>::value) cin >> *this;
   }
 
-  input &operator=(const _Tp &__x) noexcept {
-    _Tp::operator=(__x);
-    return *this;
-  }
+  template <class _E>
+  input(std::initializer_list<_E> __l) noexcept : _Tp(__l) {}
 };
 
 }  // namespace _input_impl
