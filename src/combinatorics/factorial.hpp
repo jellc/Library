844f54a54d6af16a13e5886eaa3771467dd844d1
jell <euler31415@icloud.com>
2021-10-09 12:38:59 +0900

    .

--- src/combinatorics/factorial.hpp
+++ src/combinatorics/factorial.hpp
@@ -9,8 +9,8 @@
 
 namespace workspace {
 
-// Factorial
-template <class _Tp, class _X> _Tp factorial(_X __x) noexcept {
+// Factorial.
+template <class _Tp, class _X = int_least64_t> _Tp factorial(_X __x) noexcept {
   if (__x < 0) return 0;
   static std::vector<_Tp> __t{1};
   static size_t __i = (__t.reserve(0x1000000), 0);
@@ -18,8 +18,9 @@ template <class _Tp, class _X> _Tp factorial(_X __x) noexcept {
   return __t[__x];
 }
 
-// Inverse of factorial
-template <class _Tp, class _X> _Tp inverse_factorial(_X __x) noexcept {
+// Inverse of factorial.
+template <class _Tp, class _X = int_least64_t>
+_Tp inverse_factorial(_X __x) noexcept {
   if (__x < 0) return 0;
   static std::vector<_Tp> __t{1};
   static size_t __i = (__t.reserve(0x1000000), 0);
