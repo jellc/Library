b4c04db7d14040ac7aa2b48be88c106a5cc39888
jell <euler31415@icloud.com>
2021-11-30 23:15:54 +0900

    .

--- test/library-checker/lowest_common_ancestor.test.cpp
+++ test/library-checker/lowest_common_ancestor.test.cpp
@@ -9,7 +9,7 @@
 int main() {
   int n, q;
   scanf("%d%d", &n, &q);
-  lowest_common_ancestor<> lca(n);
+  lowest_common_ancestor lca(n);
   heavy_light_decomposition hld(n);
   for (int i = 1, p; i < n; i++) {
     scanf("%d", &p);
@@ -21,7 +21,7 @@ int main() {
   for (int u, v; q--;) {
     scanf("%d%d", &u, &v);
     auto [left, right] = hld.split_path(u, v);
-    auto x = lca.get(u, v);
+    auto x = lca.query(u, v);
     auto y = hld.lca(u, v);
     assert(x == y);
     assert(x == hld.node(left.back().first));
